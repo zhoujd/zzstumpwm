e0759482d15843f15d9c28da4021f2c6536d1819
zhoujd <zjd-405@163.com>
2014-04-20 01:39:44 +0800

    format code

--- install.sh
+++ install.sh
@@ -7,11 +7,13 @@ echo "install stumpwm start ..."
 
 rm -f ~/.stumpwmrc
 ln -s $CURRENT_DIR/.stumpwmrc ~/.stumpwmrc
+ln -s $CURRENT_DIR/quicklisp ~/quicklisp
 
+## Set SBCL script
 cat > ~/.sbclrc <<EOF
 ;;; The following lines added by ql:add-to-init-file:
 #-quicklisp
-(let ((quicklisp-init (merge-pathnames "${CURRENT_DIR}/quicklisp/setup.lisp"
+(let ((quicklisp-init (merge-pathnames "quicklisp/setup.lisp"
                                        (user-homedir-pathname))))
   (when (probe-file quicklisp-init)
     (load quicklisp-init)))
