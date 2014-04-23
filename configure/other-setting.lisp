4f4181c58cca3859b3341d27cd9d514975b1ab33
zhoujd <zjd-405@163.com>
2014-04-24 01:24:53 +0800

    format code

--- configure/other-setting.lisp
+++ configure/other-setting.lisp
@@ -5,34 +5,6 @@
 (defun cat (&rest strings)
     (apply 'concatenate 'string strings))
 
-(defcommand display-current-window-info () ()
-  "Shows the properties of the current window. These properties can be
-used for matching windows with run-or-raise or window placement
--merules."
-  (let ((w (current-window))
-        (*suppress-echo-timeout* t)
-        (nl (string #\NewLine)))
-    (echo-string (current-screen)
-                 (concat "class:    " (window-class w) nl
-                         "instance: " (window-res w) nl
-                         "type:     " (string (window-type w)) nl
-                         "role:     " (window-role w) nl
-                         "title:    " (window-title w) nl
-                         "width:    " (format nil "~a" (window-width w)) nl
-                         "height    " (format nil "~a" (window-height w))))))
-
-;; sudo command
-(define-sudo-command reboot "reboot")
-(define-sudo-command shutdown "shutdown -h now")
-(define-sudo-command gnome-control-center "gnome-control-center")
-
-;; shell command
-(defcommand reinit () ()
-    (run-commands "reload" "loadrc"))
-
-;; run-or-raise
-(def-run-or-raise-command firefox '(:class "Firefox"))
-
 ;; process management
 (defun ps-exists (ps)
   (let ((f "ps -ef | grep ~S | grep -v -e grep -e stumpish | wc -l"))
@@ -50,6 +22,33 @@ used for matching windows with run-or-raise or window placement
 (defun kill-ps (command)
   (run-shell-command (kill-ps-command command)))
 
+;; sudo command
+(define-sudo-command reboot "reboot")
+(define-sudo-command shutdown "shutdown -h now")
+(define-sudo-command gnome-control-center "gnome-control-center")
+
+;; run-or-raise
+(def-run-or-raise-command firefox '(:class "Firefox"))
+
+;; shell command
+(defcommand display-current-window-info () ()
+  "Shows the properties of the current window. These properties can be
+used for matching windows with run-or-raise or window placement
+-merules."
+  (let ((w (current-window))
+        (*suppress-echo-timeout* t)
+        (nl (string #\NewLine)))
+    (echo-string (current-screen)
+                 (concat "class:    " (window-class w) nl
+                         "instance: " (window-res w) nl
+                         "type:     " (string (window-type w)) nl
+                         "role:     " (window-role w) nl
+                         "title:    " (window-title w) nl
+                         "width:    " (format nil "~a" (window-width w)) nl
+                         "height    " (format nil "~a" (window-height w))))))
+
+(defcommand reinit () ()
+    (run-commands "reload" "loadrc"))
 
 (defcommand nm-applet () ()
   (start-command-ps "nm-applet" :options "--sm-disable"))
