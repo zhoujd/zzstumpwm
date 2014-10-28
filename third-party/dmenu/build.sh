bd5fc5d6b559682d91da4cda2204af1027c984a5
zhoujd <zjd-405@163.com>
2014-10-28 09:08:30 +0800

    saving changed

--- third-party/dmenu/build.sh
+++ third-party/dmenu/build.sh
@@ -1,7 +1,6 @@
 #!/bin/sh
 
-#sudo yum install libXinerama-devel
-
+#sudo yum install libXinerama-devel libX11-devel xorg-x11-server-devel
 
 make
 sudo make install
--- third-party/xtrlock/build.sh
+++ third-party/xtrlock/build.sh
@@ -1,5 +1,7 @@
 #!/bin/sh
 
+#sudo yum install libX11-devel xorg-x11-server-devel
+
 make -f Makefile.noimake
 sudo make -f Makefile.noimake install
 
