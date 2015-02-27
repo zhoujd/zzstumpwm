a86c4b82b3842c919070dedbaa6f0852f21a17f6
zhoujd <zjd-405@163.com>
2015-02-28 06:04:58 +0800

    add more for clfswm

---
+++ clfswmrc.d/common-setting.lisp
@@ -0,0 +1,13 @@
+;;;; common setting -*-lisp-*-
+;;
+
+(in-package :clfswm)
+
+;; auto start swank server
+(require 'swank)
+(defvar *swank-server-p* nil "swant server flag")
+
+(unless *swank-server-p*
+  (swank:create-server :dont-close t :port 4405)
+  (setf swank:*use-dedicated-output-stream* nil)
+  (setf *swank-server-p* t))
