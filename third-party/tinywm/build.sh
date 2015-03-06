a4a1ebcb869f4e61aae46417420eabc43bbfadbf
zhoujd <zjd-405@163.com>
2015-03-06 11:25:35 +0800

    more saving for build tinywm

--- third-party/tinywm/build.sh
+++ third-party/tinywm/build.sh
@@ -1,6 +1,10 @@
 #!/bin/sh
 
-# http://incise.org/tinywm.html
+## http://incise.org/tinywm.html
+
+## sudo apt-get install libX11-dev
+## sudo yum install libX11-devel
+## sudo zypper install libX11-devel 
 
 echo "compile tinywm ..."
 gcc -o tinywm  tinywm.c -lX11
