6eb17cdf1180b30338847ed5c6e0b4163dc80f90
zhoujd <zjd-405@163.com>
2015-03-06 15:28:08 +0800

    add more for third-party compile

---
+++ third-party/dev-tools.sh
@@ -0,0 +1,16 @@
+#!/bin/sh
+
+case "$1" in
+    "ubuntu" )
+        sudo apt-get install libXinerama-dev libX11-dev xorg-x11-server-dev
+        ;;
+    "suse" )
+        sudo zypper install libXinerama-devel libX11-devel xorg-x11-server-devel
+        ;;
+    "centos")
+        sudo yum install libXinerama-devel libX11-devel xorg-x11-server-devel
+        ;;
+    * )
+        echo "Use $0 [ubuntu|suse|centos]"
+        ;;
+esac
