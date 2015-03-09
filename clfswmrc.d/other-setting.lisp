1f325843aeabbd301e704f15f38966be9cbe0045
zhoujd <zjd-405@163.com>
2015-03-09 23:15:01 +0800

    more for run

--- clfswmrc.d/other-setting.lisp
+++ clfswmrc.d/other-setting.lisp
@@ -3,14 +3,12 @@
 
 (in-package :clfswm)
 
-
-(defun run-shell-command (cmd)
-  (do-execute (car cmd) (cdr cmd)))
-
+;; startup run commands
 (mapc
- #'run-shell-command
+ #'(lambda (cmd)
+     (do-execute (car cmd) (cdr cmd)))
  (list
-  (list "/usr/bin/xrdb" "-load " *zz-load-directory* "misc/.Xresources")
+  (list "/usr/bin/xset" "b off")
+  (list "/usr/bin/xsetroot" "-solid black")
+  (list "/usr/bin/xrdb" "-load" *zz-load-directory* "misc/.Xresources")
   ))
-
-(do-execute "/usr/bin/xrdb" (list "-load " *zz-load-directory* "misc/.Xresources"))
