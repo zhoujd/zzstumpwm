7661e3089dbfe935b45ea6a9459f2e53ff8a3187
zhoujd <zjd-405@163.com>
2015-03-30 15:18:40 +0800

    refact urxvt

--- third-party/rxvt-unicode/build.sh
+++ third-party/rxvt-unicode/build.sh
@@ -1,7 +1,18 @@
 #!/bin/sh
 
+echo "init build environment ..."
+rm -rf build
+mkdir -p build
+
+echo "untar package ..."
+tar xf rxvt-unicode.tar.bz2 -C build
+
+pushd build/rxvt-unicode*
+
 ./configure --prefix=/usr --disable-perl
 make
-
 sudo make install
-make distclean
+
+popd
+
+echo "build rxvt-unicode ok ..."
