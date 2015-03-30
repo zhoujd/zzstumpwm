31a2726dfd22a5967a43d60b1378b8f3d1ab71de
zhoujd <zjd-405@163.com>
2015-03-30 17:02:21 +0800

    more for xtrlock

--- third-party/xtrlock/build.sh
+++ third-party/xtrlock/build.sh
@@ -2,8 +2,14 @@
 
 #sudo yum install libX11-devel xorg-x11-server-devel
 
+# compile
 make -f Makefile.noimake
+
+# install
 sudo make -f Makefile.noimake install
+
+# none root user using it
+sudo chmod u+s /usr/bin/xtrlock
 
 # clean temp file
 rm -rf xtrlock xtrlock.o
