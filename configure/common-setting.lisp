32c48efbaa1767ad28a7ad0a39c739c2c5051a70
zhoujd <zjd-405@163.com>
2015-04-23 22:56:28 +0800

    format code

--- configure/common-setting.lisp
+++ configure/common-setting.lisp
@@ -12,16 +12,14 @@
 (set-float-focus-color   "black")
 (set-float-unfocus-color "black")
 
-(setf *mode-line-border-width* 3)
-
 ;; suppress the message StumpWM displays when it starts. Set it to NIL
-(setf *startup-message*          nil
-      *suppress-frame-indicator* t
-      *suppress-abort-messages*  t
-      *timeout-wait*             3
-      *mouse-focus-policy*      :click
-      *message-window-gravity*  :center
-      *input-window-gravity*    :center)
+(setf *startup-message*             nil
+      *suppress-frame-indicator*    t
+      *suppress-abort-messages*     t
+      *timeout-wait*                3
+      *mouse-focus-policy*          :click
+      *message-window-gravity*      :center
+      *input-window-gravity*        :center)
 
 ;;; Window Appearance
 (setf *normal-border-width*         1
@@ -31,21 +29,21 @@
       *float-window-title-height*   0
       *window-border-style*         :thin)
 
-(setf *time-modeline-string* "%Y-%m-%d %a ^B%l:%M^b"
-      *window-name-source*   :title
-      *window-format*        "^B^8*%n%s%m%15t | ^7*"
-      *group-format*         "%t")
+(setf *time-modeline-string*        "%Y-%m-%d %a ^B%l:%M^b"
+      *window-name-source*          :title
+      *window-format*               "^B^8*%n%s%m%15t | ^7*"
+      *group-format*                "%t")
 
  ;;;; The Mode Line
-(setf *mode-line-background-color* "black"
-      *mode-line-foreground-color* "lightgreen"
-      *mode-line-border-color*     "black"
-      *mode-line-border-width*     0
-      *mode-line-pad-x*            0
-      *mode-line-pad-y*            0
-      *mode-line-timeout*          60
-      *mode-line-screen-position*  :top
-      *screen-mode-line-format*    (list "[^B%n^b]%W" "^> | " "^>%d"))
+(setf *mode-line-background-color*  "black"
+      *mode-line-foreground-color*  "lightgreen"
+      *mode-line-border-color*      "black"
+      *mode-line-border-width*      0
+      *mode-line-pad-x*             0
+      *mode-line-pad-y*             0
+      *mode-line-timeout*           60
+      *mode-line-screen-position*   :top
+      *screen-mode-line-format*     (list "[^B%n^b]%W" "^> | " "^>%d"))
 
 ;; turn on/off the mode line for the current screen only.
 ;(if (not (head-mode-line (current-head)))
