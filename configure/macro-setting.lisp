1459defb558d13e72761cc0221c9fa9379882004
zhoujd <zjd-405@163.com>
2015-04-23 22:29:49 +0800

    format code

--- configure/macro-setting.lisp
+++ configure/macro-setting.lisp
@@ -2,23 +2,22 @@
 
 (in-package :stumpwm)
 
-(defmacro replace-hook (hook fn)  
-  `(remove-hook ,hook ,fn)  
-  `(add-hook ,hook ,fn))  
+(defmacro replace-hook (hook fn)
+  `(remove-hook ,hook ,fn)
+  `(add-hook ,hook ,fn))
 
-; If you like Meta (most probably alt on your keyboard) more than  
-; Super (which is the Windows key on mine), change 's-' into 'M-'.  
-(defmacro defkey-top (key cmd)  
-  `(define-key *top-map* (kbd ,key) ,cmd))  
+; If you like Meta (most probably alt on your keyboard) more than
+; Super (which is the Windows key on mine), change 's-' into 'M-'.
+(defmacro defkey-top (key cmd)
+  `(define-key *top-map* (kbd ,key) ,cmd))
 
-(defmacro defkeys-top (&rest keys)  
-  (let ((ks (mapcar #'(lambda (k) (cons 'defkey-top k)) keys)))  
-    `(progn ,@ks)))  
-
-(defmacro defkey-root (key cmd)  
-  `(define-key *root-map* (kbd ,key) ,cmd))  
-  
-(defmacro defkeys-root (&rest keys)  
-  (let ((ks (mapcar #'(lambda (k) (cons 'defkey-root k)) keys)))  
+(defmacro defkeys-top (&rest keys)
+  (let ((ks (mapcar #'(lambda (k) (cons 'defkey-top k)) keys)))
     `(progn ,@ks)))
 
+(defmacro defkey-root (key cmd)
+  `(define-key *root-map* (kbd ,key) ,cmd))
+
+(defmacro defkeys-root (&rest keys)
+  (let ((ks (mapcar #'(lambda (k) (cons 'defkey-root k)) keys)))
+    `(progn ,@ks)))
