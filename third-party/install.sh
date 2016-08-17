009d757886d161f00c15636180f57f2b950bd97c
zhoujd <zjd-405@163.com>
2016-08-17 14:45:09 +0800

    more for Third party

---
+++ third-party/install.sh
@@ -0,0 +1,38 @@
+#!/bin/sh
+
+INSTALL_ROOT=`pwd`
+INSTALL_OS="unknown"
+
+case "$1" in
+    "ubuntu" )
+        INSTALL_OS=$1
+        echo "Install on $INSTALL_OS"
+        ;;
+    "suse" )
+        INSTALL_OS=$1
+        echo "Install on $INSTALL_OS"
+        ;;
+    "centos" )
+        INSTALL_OS=$1
+        echo "Install on $INSTALL_OS"
+        ;;
+    * )
+        echo "Use $0 [ubuntu|suse|centos]"
+        exit 1;
+        ;;
+esac
+
+echo "Install xtrock ..."
+cd $INSTALL_ROOT/xtrlock
+./build.sh $INSTALL_OS
+cd $INSTALL_ROOT
+
+echo "Install tmux ..."
+cd $INSTALL_ROOT/tmux
+./build.sh $INSTALL_OS
+cd $INSTALL_ROOT
+
+echo "Install dmemu ..."
+cd $INSTALL_ROOT/dmenu
+./build.sh $INSTALL_OS
+cd $INSTALL_ROOT
