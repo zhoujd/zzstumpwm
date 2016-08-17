f775862b532ee4a8cf729ce4a17a0d624a59e095
zhoujd <zjd-405@163.com>
2016-08-17 15:58:47 +0800

    * third-party/install.sh: format code

--- third-party/install.sh
+++ third-party/install.sh
@@ -1,36 +1,25 @@
 #!/bin/bash
 
-target="unknow"
-
 echo "Install third-party start ..."
 
+target=$1
+
 ## parameter check
-case "$1" in
+case "$target" in
     "ubuntu"|"suse"|"centos" )
-        target=$1
-        echo "Install on $os"
+        echo "Install on $target"
+
+        for tool in xtrlock tmux dmenu ; do
+            echo "Install $tool ..."
+            pushd $tool
+            ./install.sh $target
+            popd
+        done
         ;;
     * )
         echo "Use $0 [ubuntu|suse|centos]"
         exit 1;
         ;;
 esac
-
-## install dependence
-echo -n "Do you want to install dependence ? [yN]"
-read answer
-case "$answer" in
-    "Y" | "y" )
-        ./depends.sh $target;
-        ;;
-esac
-
-## install tool
-for tool in xtrlock tmux dmenu ; do
-    echo "Install $tool ..."
-    pushd $tool
-    ./install.sh $target
-    popd
-done
 
 echo "Install third-party end ..."
