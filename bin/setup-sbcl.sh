a0bea2700f134b67873211f57b9d52ec43315df3
zhoujd <zjd-405@163.com>
2018-04-11 20:36:55 +0800

    add setup-sbcl.sh

---
+++ bin/setup-sbcl.sh
@@ -0,0 +1,21 @@
+#!/bin/bash
+
+SCRIPT_ROOT=`pwd`
+SBCL_VERSION="1.4.6"
+SBCL_PKG="sbcl-$SBCL_VERSION-x86-64-linux-binary.tar.bz2"
+
+pushd ~/Downloads/
+
+## wget sbcl package
+rm -f $SBCL_PKG
+wget http://prdownloads.sourceforge.net/sbcl/$SBCL_PKG
+
+## untar package
+tar xf $SBCL_PKG
+
+## install sbcl to default folder (/usr/local)
+pushd sbcl-$SBCL_VERSION-x86-64-linux
+sudo sh install.sh
+popd
+
+popd
