e9e7b15ea2553d6c2de4d68fd60f52175da817d4
Zachary Zhou <zachary.zhou@hotmail.com>
2018-06-01 19:41:26 +0800

    more for swank

--- stumpwmrc.d/tool-setting.lisp
+++ stumpwmrc.d/tool-setting.lisp
@@ -5,12 +5,32 @@
 
 ;; auto start swank server
 (require 'swank)
-(defvar *swank-server-p* nil "swant server flag")
+(swank-loader:init)
+(defvar *swank-server-running* nil "swank server flag")
+(defvar *swank-server-port* 4405 "swank server port")
 
-(unless *swank-server-p*
-  (swank:create-server :port 4405
-                       :dont-close nil)
-  (setf *swank-server-p* t))
+(defun swank-start-server ()
+  (and (not *swank-server-running*)
+       (swank:create-server :port *swank-server-port*
+                            :style swank:*communication-style*
+                            :dont-close t)
+       (setf *swank-server-running* t)))
+
+(defun swank-stop-server ()
+  (and *swank-server-running*
+       (swank:stop-server *swank-server-port*)
+       (setf *swank-server-running* nil)))
+
+(defcommand start-swank-server () ()
+            "Start swank server."
+            (swank-start-server))
+
+(defcommand stop-swank-server () ()
+            "Stop swank server."
+            (swank-stop-server))
+
+;; start server
+(swank-start-server)
 
 ;; screen shot
 (require 'screenshot)
