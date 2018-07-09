bf7f74b8ef42bf7a3dabab5c87e36ca27bdca78d
Zachary Zhou <zachary.zhou@hotmail.com>
2018-07-09 05:56:16 +0000

    more for xinitrc

--- bin/xinit-setup.sh
+++ bin/xinit-setup.sh
@@ -5,17 +5,16 @@ SCRIPT_ROOT=$(cd $(dirname "${BASH_SOURCE[0]}") && pwd)
 MISC_HOME=$(cd $SCRIPT_ROOT/../misc && pwd)
 
 ## setup .xinitrc
-rm -f ~/.xinitrc
-cp $MISC_HOME/.xinitrc.in ~/.xinitrc 
+cp $MISC_HOME/.xinitrc ~/.xinitrc 
 
 ## setup .profile
 cat >> ~/.profile <<EOF
 
 # auto startx on tty1
-WM_NAME=stumpwm
-if [ "\$tty" = "/dev/tty1" ]; then
+WM_NAME="stumpwm"
+if [ "\$(tty)" = "/dev/tty1" ]; then
     WM_PID=\`ps -ef | grep \$WM_NAME | grep -v grep | awk '{print \$2}'\`
-    if [ "" = "\$WM_PID" ]; then
+    if [ -z "\$WM_PID" ]; then
         startx
     fi
 fi
