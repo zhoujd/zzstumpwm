fceb88c028653657408f4591a0028ad1189f0764
Zachary Zhou <zachary.zhou@hotmail.com>
2018-07-17 06:32:43 +0800

    add more for lightdm

---
+++ bin/light-dm-setup.sh
@@ -0,0 +1,22 @@
+#!/bin/bash
+
+## lightdm install
+#sudo pacman -S lightdm lightdm-gtk-greeter
+#rm ~/.Xauthority
+
+CURRENT_DIR=`pwd`
+
+if [ $EUID -ne 0 ]; then
+    echo "You must be a root user" 2>&1
+    exit 1
+fi
+
+echo "[Desktop Entry]
+Version=1.0
+Name=Stumpwm Session
+Comment=Stumpwm window manager
+Exec=/home/zhoujd/zzstumpwm/bin/start-wm.sh
+Icon=
+Type=Application
+DesktopNames=STUMPWM
+" > /usr/share/xsessions/stumpwm.desktop
