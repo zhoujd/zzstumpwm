b3101b1e76f63ab0e5c1f9740acf3ea28419035a
Zachary Zhou <zachary.zhou@hotmail.com>
2018-09-04 17:05:33 +0800

    format code

--- stumpwmrc.d/key-setting.lisp
+++ stumpwmrc.d/key-setting.lisp
@@ -7,73 +7,73 @@
 (apply-keys-to-map
  *zz-exec-map*
  (list
-  (kbd "c")         "urxvt"
-  (kbd "C-c")       "exec urxvt"
-  (kbd "e")         "runemacs"
-  (kbd "s-e")       "emacs"
-  (kbd "C-e")       "exec emacs"
-  (kbd "f")         "firefox"
-  (kbd "C-f")       "exec firefox"
-  (kbd "m")         "exec xfce4-taskmanager"
-  (kbd "h")         "exec thunar"
-  (kbd "t")         (urxvt-command "tmux")
-  (kbd "v")         "exec evince"
-  (kbd "r")         "exec remmina"
-  (kbd "b")         "colon1 exec firefox http://www."
-  (kbd "s")         "colon1 exec urxvt -e ssh "
+  (kbd "c")           "urxvt"
+  (kbd "C-c")         "exec urxvt"
+  (kbd "e")           "runemacs"
+  (kbd "s-e")         "emacs"
+  (kbd "C-e")         "exec emacs"
+  (kbd "f")           "firefox"
+  (kbd "C-f")         "exec firefox"
+  (kbd "m")           "exec xfce4-taskmanager"
+  (kbd "h")           "exec thunar"
+  (kbd "t")           (urxvt-command "tmux")
+  (kbd "v")           "exec evince"
+  (kbd "r")           "exec remmina"
+  (kbd "b")           "colon1 exec firefox http://www."
+  (kbd "s")           "colon1 exec urxvt -e ssh "
   ))
 
 ;; *zz-x-map*
 (apply-keys-to-map
  *zz-x-map*
  (list
-  (kbd "1")         "only"
-  (kbd "2")         "vsplit"
-  (kbd "3")         "hsplit"
-  (kbd "4")         "kill-window"
-  (kbd "5")         "fselect"
-  (kbd "6")         "balance-frames"
-  (kbd "7")         "refresh"
-  (kbd "8")         "delete-window"
-  (kbd "9")         "iresize"
-  (kbd "0")         "remove-split"
-  (kbd "-")         "dump-desktop-to-file ~/.stumpwm-desktop"
-  (kbd "=")         "restore-from-file ~/.stumpwm-desktop"
+  (kbd "1")           "only"
+  (kbd "2")           "vsplit"
+  (kbd "3")           "hsplit"
+  (kbd "4")           "kill-window"
+  (kbd "5")           "fselect"
+  (kbd "6")           "balance-frames"
+  (kbd "7")           "refresh"
+  (kbd "8")           "delete-window"
+  (kbd "9")           "iresize"
+  (kbd "0")           "remove-split"
+  (kbd "-")           "dump-desktop-to-file ~/.stumpwm-desktop"
+  (kbd "=")           "restore-from-file ~/.stumpwm-desktop"
 
-  (kbd "i")         "current-window-info"
-  (kbd "v")         "version"
-  
+  (kbd "i")           "current-window-info"
+  (kbd "v")           "version"
+
   ;; exchange-direction bound to C-t x by defalut
-  (kbd "Left")      "exchange-direction left"
-  (kbd "Right")     "exchange-direction right"
-  (kbd "Up")        "exchange-direction up"
-  (kbd "Down")      "exchange-direction down"
+  (kbd "Left")        "exchange-direction left"
+  (kbd "Right")       "exchange-direction right"
+  (kbd "Up")          "exchange-direction up"
+  (kbd "Down")        "exchange-direction down"
   ))
 
 ;; *zz-help-map*
 (apply-keys-to-map
  *zz-help-map*
  (list
-  (kbd "m")         (format nil "exec urxvt -e info ~a"
-                            (merge-pathnames "doc/stumpwm.info" *zz-load-directory*))
-  (kbd "C-m")       "exec firefox http://stumpwm.org/manual/stumpwm.html"
-  (kbd "s")         "exec firefox http://lxr.free-electrons.com/"
-  (kbd "C-s")       "exec firefox http://svnweb.freebsd.org/"
-  (kbd "v")         "describe-variable"
-  (kbd "f")         "describe-function"
-  (kbd "k")         "describe-key"
-  (kbd "w")         "where-is"
+  (kbd "m")           (format nil "exec urxvt -e info ~a"
+                              (merge-pathnames "doc/stumpwm.info" *zz-load-directory*))
+  (kbd "C-m")         "exec firefox http://stumpwm.org/manual/stumpwm.html"
+  (kbd "s")           "exec firefox http://lxr.free-electrons.com/"
+  (kbd "C-s")         "exec firefox http://svnweb.freebsd.org/"
+  (kbd "v")           "describe-variable"
+  (kbd "f")           "describe-function"
+  (kbd "k")           "describe-key"
+  (kbd "w")           "where-is"
   ))
 
 ;; input map
 (apply-keys-to-map
  *input-map*
  (list
-  (kbd "C-m")       'input-submit
-  (kbd "C-h")       'input-delete-backward-char
-  (kbd "M-p")       'input-history-back
-  (kbd "M-n")       'input-history-forward
-  (kbd "C-i")       'input-complete-forward
+  (kbd "C-m")         'input-submit
+  (kbd "C-h")         'input-delete-backward-char
+  (kbd "M-p")         'input-history-back
+  (kbd "M-n")         'input-history-forward
+  (kbd "C-i")         'input-complete-forward
   ))
 
 ;; top map
@@ -84,7 +84,7 @@
   (kbd "s-C-`")       "grouplist"
   (kbd "s-Escape")    "abort"
   (kbd "s-C-Escape")  "banish"
-  
+
   (kbd "s-l")         "exec xtrlock"
   (kbd "s-C-l")       "exec slock"
   (kbd "s-M-l")       "exec xset dpms force suspend"
