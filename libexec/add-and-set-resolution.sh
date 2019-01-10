90056f50b5b439754281057aaf212706a796e8f6
Zachary Zhou <zachary.zhou@hotmail.com>
2019-01-10 08:06:59 +0800

    add more for resolution

---
+++ libexec/add-and-set-resolution.sh
@@ -0,0 +1,22 @@
+#!/bin/bash
+#set -x
+
+output="$1"
+x="$2"
+y="$3"
+freq="$4"
+
+if [ $# -ne 4 ]; then
+echo "Usage: $0 output x y freq"
+echo "To find output name: xrandr -q"
+exit 0
+fi
+
+mode=$( cvt "$x" "$y" "$freq" | grep -v '^#' | cut -d' ' -f3- )
+modename="${x}x${y}"
+
+xrandr --newmode $modename $mode
+xrandr --addmode "$output" "$modename"
+xrandr --output "$output" --mode "$modename"
+
+exit 0
