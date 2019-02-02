034c1579aa58f7ce14c44685a08e344d097ceded
Zachary Zhou <zachary.zhou@hotmail.com>
2019-02-02 14:30:48 +0800

    add more for auto start x from tty1 login

---
+++ bin/profile_setup.sh
@@ -0,0 +1,11 @@
+#!/bin/bash
+#set -x -v
+
+SCRIPT_ROOT=$(cd $(dirname "${BASH_SOURCE[0]}") && pwd)
+
+## setup .profile or .bash_profile
+cat <<EOF >> ~/.bash_profile
+[[ -f ~/.tprofile ]] &&  ~/.tprofile
+EOF
+
+echo "profile_setup done"
