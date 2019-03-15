62b12968ca41126088030fb7803e9c8807881f98
Zachary Zhou <zachary.zhou@hotmail.com>
2019-03-15 09:30:51 +0800

    clean code for start-wm.sh

--- bin/start-wm.sh
+++ bin/start-wm.sh
@@ -1,6 +1,6 @@
 #!/bin/sh
 
-## Select window manager to run
+## Select window manager
 ## 1:stumpwm 2:clfswm
 echo "You can also run : $0 [1|2]"
 echo "1:stumpwm"
@@ -9,18 +9,17 @@ echo "2:clfswm"
 ## Here stumpwm is kept as default
 WM_SELECT=${1:-1}
 
-# cp ~/zzstumpwm/misc/.xprofile ~
-if [ -x ~/.xprofile ]; then
-    ~/.xprofile
-fi
+## Run .xprofile
+## cp ~/zzstumpwm/misc/.xprofile ~
+test -x ~/.xprofile && ~/.xprofile
 
+## Start window manager
 case "$WM_SELECT" in
     "1" )
         exec sbcl --eval '(ql:quickload :stumpwm)' \
                   --eval '(stumpwm:stumpwm)'
         ;;
     "2" )
-	    # https://trac.common-lisp.net/clfswm/wiki
         exec sbcl --eval '(ql:quickload :clfswm)' \
                   --eval '(clfswm:main)'
         ;;
