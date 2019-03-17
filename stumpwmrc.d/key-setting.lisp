9016bcaa07475cfda960d14db2fa473d040fab43
Zachary Zhou <zachary.zhou@hotmail.com>
2019-03-17 20:42:02 +0800

    add more for wifi

--- stumpwmrc.d/key-setting.lisp
+++ stumpwmrc.d/key-setting.lisp
@@ -29,6 +29,7 @@
   (kbd "s")             "colon1 exec urxvt -e ssh "
   (kbd "C-s")           (urxvt-command "speedpad")
   (kbd "M-s")           "stumpish"
+  (kbd "w")             "exec wicd-client"
   ))
 
 ;; *zz-x-map*
