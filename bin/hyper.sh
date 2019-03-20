d261b481d73c135e8b17e78976881e2d60094e01
Zachary Zhou <zachary.zhou@hotmail.com>
2019-03-20 11:12:34 +0800

    add more for hyper setup

--- bin/hyper.sh
+++ bin/hyper.sh
@@ -5,17 +5,17 @@ SCRIPT_ROOT=$(cd $(dirname "${BASH_SOURCE[0]}") && pwd)
 ZZSTUMPWM_ROOT=$(cd $SCRIPT_ROOT/.. && pwd)
 
 case "$1" in
-    "default" )
-        echo "default key layout"
-        setcapslock off
-        xmodmap $ZZSTUMPWM_ROOT/misc/.xmodmap/default.xmodmap
-        ;;
-    "hyper" )
+    "on" )
         echo "hyper key layout"
         setcapslock off
         xmodmap $ZZSTUMPWM_ROOT/misc/.xmodmap/hyper.xmodmap
         ;;
+    "off" )
+        echo "default key layout"
+        setcapslock off
+        xmodmap $ZZSTUMPWM_ROOT/misc/.xmodmap/default.xmodmap
+        ;;
     * )
-        echo "$(basename $0) {default|hyper}"
+        echo "$(basename $0) {on|off}"
         ;;
 esac
