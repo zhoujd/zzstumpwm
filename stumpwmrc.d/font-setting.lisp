198697f48aa825beb5b69775606509c95764f813
Zachary Zhou <zachary.zhou@hotmail.com>
2019-04-02 17:57:03 +0800

    more for font setting

--- stumpwmrc.d/font-setting.lisp
+++ stumpwmrc.d/font-setting.lisp
@@ -3,9 +3,8 @@
 
 (in-package :stumpwm)
 
-(require 'ttf-fonts)
-
 (ignore-errors
+  (require 'ttf-fonts)
   (or (and (xft:cache-fonts)
            (set-font (make-instance 'xft:font :family "Anonymous Pro" :subfamily "Regular" :size 14)))
       (set-font "10x20")))
