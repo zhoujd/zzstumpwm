5982a5647e9425e56224fb4ab597c2ab4d10741b
Zachary Zhou <zachary.zhou@hotmail.com>
2019-04-11 08:20:12 +0800

    format code

--- libexec/brightness-setup.sh
+++ libexec/brightness-setup.sh
@@ -1,8 +1,14 @@
 #!/bin/bash
 
+# Check for root user
+if [ $EUID -ne 0 ]; then
+    echo "You must be a root user" 2>&1
+    exit 1
+fi
+
 # Simple script to modify screen brightness
 basedir="/sys/class/backlight/"
 
 # get the backlight handler
 handler=$basedir$(ls $basedir)"/"
-sudo chmod 666 $handler"brightness"
+chmod 666 $handler"brightness"
