e2440eb0b65323ca1d90049da9f1779e85c038c8
Zachary Zhou <zachary.zhou@hotmail.com>
2019-12-16 16:03:30 +0800

    update for hyper

--- bin/hyper-setup.sh
+++ bin/hyper-setup.sh
@@ -4,11 +4,6 @@ SCRIPT_ROOT=$(cd $(dirname "${BASH_SOURCE[0]}") && pwd)
 ZZSTUMPWM_ROOT=$(cd $SCRIPT_ROOT/.. && pwd)
 
 BIN_TARGET=/usr/bin
-sudo tee $BIN_TARGET/hyper <<EOF
-#/bin/bash
-$ZZSTUMPWM_ROOT/bin/hyper \$*
-EOF
-
-sudo chmod +x $BIN_TARGET/hyper
+sudo ln -sfv $ZZSTUMPWM_ROOT/bin/hyper $BIN_TARGET
 
 echo "setup $BIN_TARGET/hyper done"
