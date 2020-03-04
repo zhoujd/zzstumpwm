4cb3d51df3e5960a5d174a21eef06b1a20111fa0
Zachary Zhou <zchrzhou@gmail.com>
2020-03-04 14:30:17 +0800

    bin: add more for xprofile

--- bin/xprofile-setup.sh
+++ bin/xprofile-setup.sh
@@ -5,6 +5,6 @@ SCRIPT_ROOT=$(cd $(dirname "${BASH_SOURCE[0]}") && pwd)
 ZZSTUMPWM_ROOT=$(cd $SCRIPT_ROOT/.. && pwd)
 
 ## setup .xprofile
-sudo ln -sfv $ZZSTUMPWM_ROOT/misc/.xprofile /etc/profile.d/zz-xprofile
+sudo ln -sfv $ZZSTUMPWM_ROOT/misc/.xprofile /etc/profile.d/zz-xprofile.sh
 
 echo "xprofile_setup done"
