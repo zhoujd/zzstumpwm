f77d974da33a99f058019fe8726ac1fd6ed6d7aa
Zachary Zhou <zchrzhou@gmail.com>
2020-03-04 21:19:28 +0800

    more for xprofile

--- bin/xprofile-setup.sh
+++ bin/xprofile-setup.sh
@@ -3,8 +3,55 @@
 
 SCRIPT_ROOT=$(cd $(dirname "${BASH_SOURCE[0]}") && pwd)
 ZZSTUMPWM_ROOT=$(cd $SCRIPT_ROOT/.. && pwd)
+ZZXPROFILE_PATH=/usr/bin/zzxprofile
 
-## setup .xprofile
-sudo ln -sfv $ZZSTUMPWM_ROOT/misc/.xprofile /etc/profile.d/zz-xprofile.sh
+install_zzxprofile() {
+    sudo tee $ZZXPROFILE_PATH <<EOF
+## zz xprofile
+$ZZSTUMPWM_ROOT/misc/.xprofile
+EOF
+    sudo chmod +x $ZZXPROFILE_PATH
+}
+
+install_auto() {
+    local xprofile_rc=z02xprofile
+    if [ -d /etc/rc.zach.d ]; then
+        sudo tee /etc/rc.zach.d/$xprofile_rc <<EOF
+#!/bin/bash
+
+do_start() {
+    echo "\$0 start"
+    test -x $ZZXPROFILE_PATH && $ZZXPROFILE_PATH
+}
+
+do_stop() {
+    echo "\$0 stop"
+}
+
+case "\$1" in
+    start)
+    	do_start
+        ;;
+    stop)
+        do_stop
+        ;;
+    *)
+        echo "Usage: \$0 {start|stop}" >&2
+        exit 3
+        ;;
+esac
+exit 0
+EOF
+        sudo chmod +x /etc/rc.zach.d/$xprofile_rc
+    else
+        echo "Install /etc/rc.zach.d is not exist"
+    fi
+    
+    echo "Install zzxprofile auto done"
+
+}
+
+install_zzxprofile
+install_auto
 
 echo "xprofile_setup done"
