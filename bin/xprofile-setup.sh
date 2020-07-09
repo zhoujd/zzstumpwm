8ae457b56943771e3b916645e67fd9947f08ea64
Zachary Zhou <zchrzhou@gmail.com>
2020-07-09 17:31:36 +0800

    add xprofile

--- bin/xprofile-setup.sh
+++ bin/xprofile-setup.sh
@@ -3,12 +3,12 @@
 
 SCRIPT_ROOT=$(cd $(dirname "${BASH_SOURCE[0]}") && pwd)
 ZZSTUMPWM_ROOT=$(cd $SCRIPT_ROOT/.. && pwd)
-ZZXPROFILE_PATH=/usr/bin/zzxprofile
+XPROFILE_PATH=/usr/bin/xprofile
 
-install_zzxprofile() {
-    sudo ln -sf $ZZSTUMPWM_ROOT/misc/.xprofile $ZZXPROFILE_PATH
+install_xprofile() {
+    sudo ln -sfv $ZZSTUMPWM_ROOT/misc/.xprofile $XPROFILE_PATH
 }
 
-install_zzxprofile
+install_xprofile
 
 echo "xprofile_setup done"
--- stumpwmrc.d/other-setting.lisp
+++ stumpwmrc.d/other-setting.lisp
@@ -46,4 +46,5 @@ Returns true when yes is selected"
  (list
   (list "xset" :options "b off")
   (list "xsetroot" :options "-cursor_name left_ptr -solid black -name root_window")
+  (list "xprofile")
   ))
