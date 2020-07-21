a61adb1358efdbac8ad9f67b0ce9ecaffffcf78b
Zachary Zhou <zchrzhou@gmail.com>
2020-07-21 11:01:12 +0800

    more for gtk icon/theme setting

--- bin/gtk2-setup.sh
+++ bin/gtk2-setup.sh
@@ -1,2 +1,13 @@
 #!/bin/bash
 
+### Setting example for numix or Tango
+## ln -sf `pwd`/.gtkrc-2.0 ~
+## apt search icon-theme
+## sudo pacman -Ss icon-theme
+## sudo apt install tango-icon-theme numix-icon-theme
+
+rm -fv ~/.gtkrc-2.0
+tee ~/.gtkrc-2.0 <<EOF
+gtk-theme-name = "zach-theme"
+gtk-icon-theme-name = "MacBuntu-OS"
+EOF
--- bin/gtk3-setup.sh
+++ bin/gtk3-setup.sh
@@ -1,2 +1,35 @@
 #!/bin/bash
 
+### Ubuntu 20.04 Yaru theme, icon and sound
+## sudo apt install yaru-theme-gtk yaru-theme-icon yaru-theme-sound
+
+install_user() {
+    echo "Install gtk3 config to user"
+
+    local target=~/.config/gtk-3.0/settings.ini
+    tee $target <<EOF
+[Settings]
+gtk-theme-name = zach-theme
+gtk-icon-theme-name = Os-Catalina
+EOF
+}
+
+install_sys() {
+    echo "Install gtk3 config to $target"
+
+    local target=/etc/gtk-3.0/settings.ini
+    sudo sed -i 's/gtk-theme-name.*/gtk-theme-name = Yaru/' $target
+    sudo sed -i 's/gtk-icon-theme-name.*/gtk-icon-theme-name = Yaru/' $target
+}
+
+case $1 in
+    user )
+        install_user
+        ;;
+    sys )
+        install_sys
+        ;;
+    * )
+        echo "Usage: $0 {user|sys}"
+        ;;
+esac
