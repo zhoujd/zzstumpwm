0d4eb96f85af3779384be1ed35898a112d426bf7
Zachary Zhou <zchrzhou@gmail.com>
2020-12-01 14:25:47 +0800

    restore zlock to slock

--- stumpwmrc.d/other-setting.lisp
+++ stumpwmrc.d/other-setting.lisp
@@ -46,6 +46,6 @@ Returns true when yes is selected"
  (list
   (list "/usr/bin/xset" :options "b off")
   (list "/usr/bin/xsetroot" :options "-cursor_name left_ptr -solid black -name root_window")
-  (list "/usr/bin/xautolock" :options "-time 15 -locker zlock")
+  (list "/usr/bin/xautolock" :options "-time 15 -locker slock")
   (list "/usr/bin/xprofile")
   ))
