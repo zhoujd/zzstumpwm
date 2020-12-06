410a970a5457ebaa23519ca9ab614eb6a166429b
Zachary Zhou <zachary.zhou@intel.com>
2020-12-06 16:47:52 +0800

    more for hyper

--- bin/hyper-setup.sh
+++ bin/hyper-setup.sh
@@ -3,7 +3,19 @@
 SCRIPT_ROOT=$(cd $(dirname "${BASH_SOURCE[0]}") && pwd)
 ZZSTUMPWM_ROOT=$(cd $SCRIPT_ROOT/.. && pwd)
 
-BIN_TARGET=/usr/bin
-sudo ln -sfv $ZZSTUMPWM_ROOT/bin/hyper $BIN_TARGET
+Install_hyper() {
+    local bin_target=/usr/bin
+    local bin_name=hyper
+    echo "Install hyper to $BIN_TARGET"
+    sudo ln -sfvT $ZZSTUMPWM_ROOT/bin/$bin_name $bin_target/$bin_name
+}
+
+Install_xmodmap() {
+    echo "Install .Xmodmap.d to $HOME"
+    ln -sfvT $ZZSTUMPWM_ROOT/misc/.Xmodmap.d ~/.Xmodmap.d
+}
+
+Install_hyper
+Install_xmodmap
 
 echo "setup $BIN_TARGET/hyper done"
