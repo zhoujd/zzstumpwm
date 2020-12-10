8bf5ff1b9c6891cfb1966f0c88797f99e53d6eac
Zachary Zhou <zachary.zhou@intel.com>
2020-12-10 13:31:22 +0800

    add more comments

--- third-party/slock/config.def.h
+++ third-party/slock/config.def.h
@@ -5,7 +5,7 @@ static const char *group = "nogroup";
 static const char *colorname[NUMCOLS] = {
 	[INIT] =   "black",     /* after initialization */
 	[INPUT] =  "#005577",   /* during input */
-	[FAILED] = "black",     /* wrong password */
+	[FAILED] = "black",     /* wrong password #CC3333 -> black */
 };
 
 /* treat a cleared input like a wrong password (color) */
