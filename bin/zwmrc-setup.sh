0dcf02b523e5b81d6e44dca8774d23d63565a65f
Zachary Zhou <zchrzhou@gmail.com>
2021-05-11 16:42:27 +0800

    add more for zwm

--- bin/zwmrc-setup.sh
+++ bin/zwmrc-setup.sh
@@ -3,13 +3,27 @@
 
 SCRIPT_ROOT=$(cd $(dirname "${BASH_SOURCE[0]}") && pwd)
 ZZSTUMPWM_ROOT=$(cd $SCRIPT_ROOT/.. && pwd)
-ZWMRC_PATH=$HOME/.zwmrc
 
 install_zwmrc() {
     ln -sfvT $ZZSTUMPWM_ROOT/misc/.zwmrc $HOME/.zwmrc
     ln -sfvT $ZZSTUMPWM_ROOT/misc/.zwmrc.d $HOME/.zwmrc.d
 }
 
+install_zwmrc_load() {
+    local target=/usr/bin/zwm-load
+    sudo tee $target <<EOF
+#!/bin/sh
+
+if [ -f ~/.zwmrc ]; then
+    . ~/.zwmrc
+else
+    echo "Cannot find ~/.zwmrc"
+fi
+EOF
+    sudo chmod +x $target
+}
+
 install_zwmrc
+install_zwmrc_load
 
 echo "zwmrc_setup done"
