5eeb492b28733f19e13e90650cd076a7095c6e86
Zachary Zhou <zchrzhou@gmail.com>
2021-05-17 22:17:55 +0800

    clean for laptop script

--- misc/.zwmrc.d/02_laptop.sh
+++ misc/.zwmrc.d/02_laptop.sh
@@ -1,28 +1,29 @@
 #!/bin/bash
 
-## laptop multi-screen
 laptop_screen() {
-    ##mirror ext monitor
     local laptop="eDP-1"
     local ext="DP-1"
     local flag=`xrandr | grep "^$ext" | awk '{print $2}'`
     local size="1920x1080"
     if [ "$flag" = "connected" ]; then
+        echo "[laptop] mirror ext monitor"
         xrandr --addmode $ext $size
         xrandr --output $ext --same-as $laptop --mode $size
     fi
 }
 
-## laptop keyboard
 laptop_keyboard() {
-    ##load Xmodmap for laptop
-    test -f ~/.Xmodmap && xmodmap ~/.Xmodmap
+    if [ -f ~/.Xmodmap ]; then
+        echo "[laptop] load .Xmodmap for laptop"
+        xmodmap ~/.Xmodmap
+    fi
 }
 
-## laptop touchpad
 laptop_touchpad() {
-    ##disable tap to click
-    synclient TapButton1=0 >/dev/null 2>&1
+    if [ -n "$(xinput list | grep -i touchpad)" ]; then
+        echo "[laptop] disable tap to click"
+        synclient TapButton1=0
+    fi           
 }
 
 laptop_screen
