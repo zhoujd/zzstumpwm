643c269342888b60093e12bf7e3718b294ecf109
Zachary Zhou <zchrzhou@gmail.com>
2021-05-17 22:09:05 +0800

    clean code for zwmrc

--- misc/.zwmrc.d/02_laptop.sh
+++ misc/.zwmrc.d/02_laptop.sh
@@ -2,12 +2,12 @@
 
 ## laptop multi-screen
 laptop_screen() {
+    ##mirror ext monitor
     local laptop="eDP-1"
     local ext="DP-1"
-    local ext_flag=`xrandr | grep "^$ext" | awk '{print $2}'`
+    local flag=`xrandr | grep "^$ext" | awk '{print $2}'`
     local size="1920x1080"
-
-    if [ "$ext_flag" = "connected" ]; then
+    if [ "$flag" = "connected" ]; then
         xrandr --addmode $ext $size
         xrandr --output $ext --same-as $laptop --mode $size
     fi
@@ -15,19 +15,14 @@ laptop_screen() {
 
 ## laptop keyboard
 laptop_keyboard() {
-    local target=~/.Xmodmap
-    if [ -f $target ]; then
-        xmodmap $target
-        echo "Load $target done"
-    else
-        echo "Cannot find $target"
-    fi
+    ##load Xmodmap for laptop
+    test -f ~/.Xmodmap && xmodmap ~/.Xmodmap
 }
 
 ## laptop touchpad
 laptop_touchpad() {
-    # disable tap to click
-    synclient TapButton1=0
+    ##disable tap to click
+    synclient TapButton1=0 >/dev/null 2>&1
 }
 
 laptop_screen
