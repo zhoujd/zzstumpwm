fb3a73276194583be8150a3f096bf90df9f2dd85
Zachary Zhou <zchrzhou@gmail.com>
2021-05-17 20:52:18 +0800

    clean script

--- misc/.zwmrc.d/02_laptop.sh
+++ misc/.zwmrc.d/02_laptop.sh
@@ -3,14 +3,9 @@
 ## laptop multi-screen
 laptop_screen() {
     local laptop="eDP-1"
-    local laptop_flag=`xrandr | grep "^$laptop" | awk '{print $2}'`
     local ext="DP-1"
     local ext_flag=`xrandr | grep "^$ext" | awk '{print $2}'`
     local size="1920x1080"
-
-    if [ "$laptop_flag" = "connected" ]; then
-        xrandr --output $laptop --primary
-    fi
 
     if [ "$ext_flag" = "connected" ]; then
         xrandr --addmode $ext $size
