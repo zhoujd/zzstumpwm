2e47a98c523ff456b6722a441b73cda9dec8fe09
Zachary Zhou <zchrzhou@gmail.com>
2021-05-18 09:23:29 +0800

    clean code for install.sh

--- install.sh
+++ install.sh
@@ -8,85 +8,61 @@ echo "install stumpwm start ..."
 mkdir -p ~/common-lisp
 
 ## Set configure file and quicklisp
-rm -f ~/.stumpwmrc
-ln -s $CURRENT_DIR/.stumpwmrc ~/.stumpwmrc
-
-rm -f ~/.clfswmrc
-ln -s $CURRENT_DIR/.clfswmrc ~/.clfswmrc
-
-rm -f ~/quicklisp
-ln -s $CURRENT_DIR/quicklisp ~/quicklisp
-
-rm -f ~/lisp
-ln -s $CURRENT_DIR/lisp ~/lisp
+ln -sfvT $CURRENT_DIR/.stumpwmrc ~/.stumpwmrc
+ln -sfvT $CURRENT_DIR/.clfswmrc ~/.clfswmrc
+ln -sfvT $CURRENT_DIR/quicklisp ~/quicklisp
+ln -sfvT $CURRENT_DIR/lisp ~/lisp
 
 ## Set SBCL configre
-rm -f ~/.sbclrc
-ln -s $CURRENT_DIR/misc/.sbclrc ~/.sbclrc
+ln -sfvT $CURRENT_DIR/misc/.sbclrc ~/.sbclrc
 
 ## Set ECL configure
-rm -f ~/.eclrc
-ln -s $CURRENT_DIR/misc/.eclrc ~/.eclrc
+ln -sfvT $CURRENT_DIR/misc/.eclrc ~/.eclrc
 
 ## Set CLISP configure
-rm -f ~/.clisprc
-ln -s $CURRENT_DIR/misc/.clisprc ~/.clisprc
+ln -sfvT $CURRENT_DIR/misc/.clisprc ~/.clisprc
 
 ## Set Vim configure
-rm -f ~/.vimrc
-ln -s $CURRENT_DIR/misc/.vimrc ~/.vimrc
+ln -sfvT $CURRENT_DIR/misc/.vimrc ~/.vimrc
 
 ## Set Nano configure
-rm -f ~/.nanorc
-ln -s $CURRENT_DIR/misc/.nanorc ~/.nanorc
+ln -sfvT $CURRENT_DIR/misc/.nanorc ~/.nanorc
 
 ## Set tmux configure
-rm -f ~/.tmux.conf
-ln -s $CURRENT_DIR/misc/.tmux.conf ~/.tmux.conf
+ln -sfvT $CURRENT_DIR/misc/.tmux.conf ~/.tmux.conf
 
 ## Set byobu configure
-rm -f ~/.byobu
-ln -s $CURRENT_DIR/misc/.byobu ~/.byobu
+ln -sfvT $CURRENT_DIR/misc/.byobu ~/.byobu
 
 ## Set csh configure
-rm -f ~/.cshrc
-ln -s $CURRENT_DIR/misc/.cshrc ~/.cshrc
+ln -sfvT $CURRENT_DIR/misc/.cshrc ~/.cshrc
 
 ## Install .Xresources
-rm -f ~/.Xresources
-ln -s $CURRENT_DIR/misc/.Xresources ~/.Xresources
+ln -sfvT $CURRENT_DIR/misc/.Xresources ~/.Xresources
 
 ## Install .Xdefaults
-rm -f ~/.Xdefaults
-ln -s $CURRENT_DIR/misc/.Xdefaults ~/.Xdefaults
+ln -sfvT $CURRENT_DIR/misc/.Xdefaults ~/.Xdefaults
 
 ## Install .inputrc
-rm -f ~/.inputrc
-ln -s $CURRENT_DIR/misc/.inputrc ~/.inputrc
+ln -sfvT $CURRENT_DIR/misc/.inputrc ~/.inputrc
 
 ## Install .urxvt
-rm -f ~/.urxvt
-ln -s $CURRENT_DIR/misc/.urxvt ~/.urxvt
+ln -sfvT $CURRENT_DIR/misc/.urxvt ~/.urxvt
 
 ## Install .xrdb
-rm -f ~/.xrdb
-ln -s $CURRENT_DIR/misc/.xrdb ~/.xrdb
+ln -sfvT $CURRENT_DIR/misc/.xrdb ~/.xrdb
 
 ## Install .xinitrc
-rm -f ~/.xinitrc
-ln -s $CURRENT_DIR/misc/.xinitrc ~/.xinitrc
+ln -sfvT $CURRENT_DIR/misc/.xinitrc ~/.xinitrc
 
 ## Install .xsession
-rm -f ~/.xsession
-ln -s $CURRENT_DIR/misc/.xsession ~/.xsession
+ln -sfvT $CURRENT_DIR/misc/.xsession ~/.xsession
 
 ## Install .keynavrc
-rm -f ~/.keynavrc
-ln -s $CURRENT_DIR/misc/.keynavrc ~/.keynavrc
+ln -sfvT $CURRENT_DIR/misc/.keynavrc ~/.keynavrc
 
 ## Install rofi
 mkdir -p ~/.config
-rm -f ~/.config/rofi
-ln -s $CURRENT_DIR/misc/.config/rofi ~/.config/rofi
+ln -sfvT $CURRENT_DIR/misc/.config/rofi ~/.config/rofi
 
 echo "install stumpwm end ..."
