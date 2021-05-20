57ed83c4b7e7b0c7819ed62502772b38cd15dc12
Zachary Zhou <zchrzhou@gmail.com>
2021-05-20 23:43:03 +0800

    more for font size to 13

--- stumpwmrc.d/font-setting.lisp
+++ stumpwmrc.d/font-setting.lisp
@@ -6,5 +6,5 @@
 (ignore-errors
   (require 'ttf-fonts)
   (or (and (xft:cache-fonts)
-           (set-font (make-instance 'xft:font :family "WenQuanYi Micro Hei Mono" :subfamily "Regular" :size 14)))
+           (set-font (make-instance 'xft:font :family "WenQuanYi Micro Hei Mono" :subfamily "Regular" :size 13)))
       (set-font "10x20")))
