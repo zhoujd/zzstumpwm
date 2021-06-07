7cd8c88bd4b46a06f8494c5f70e470212af67058
Zachary Zhou <zchrzhou@gmail.com>
2021-06-07 16:35:14 +0800

    clean code

--- bin/lightdm-setup.sh
+++ bin/lightdm-setup.sh
@@ -13,17 +13,8 @@ fi
 echo "[Desktop Entry]
 Name=Stumpwm
 Comment=Stumpwm window manager
-Exec=$SETUP_ROOT/zwm-session 1
+Exec=$SETUP_ROOT/zwm-session
 Icon=
 Type=Application
 DesktopNames=STUMPWM
 " > /usr/share/xsessions/stumpwm.desktop
-
-echo "[Desktop Entry]
-Name=clfswm
-Comment=clfswm window manager
-Exec=$SETUP_ROOT/zwm-session 2
-Icon=
-Type=Application
-DesktopNames=CLFSWM
-" > /usr/share/xsessions/clfswm.desktop
