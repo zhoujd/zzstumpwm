71444f07766eb430b0f10b9b390f7d3350791547
Zachary Zhou <zchrzhou@gmail.com>
2021-07-17 21:51:08 +0800

    clean code

--- stumpwmrc.d/key-setting.lisp
+++ stumpwmrc.d/key-setting.lisp
@@ -201,8 +201,6 @@
   (kbd "s-p")           "screen-menu"
   (kbd "s-t")           "trans-shell"
   (kbd "s-M-t")         "trans-command"
-  (kbd "s-q")           "albert"
-  (kbd "s-M-q")         "slingscold"
   (kbd "s-M-z")         "mobl-raise"
   ))
 
