58798922fec2c09fcc80ec6ead231b08e4a6c01e
Zachary Zhou <zchrzhou@gmail.com>
2021-08-01 10:21:24 +0800

    more for app menu

--- stumpwmrc.d/menu-setting.lisp
+++ stumpwmrc.d/menu-setting.lisp
@@ -5,29 +5,29 @@
 
 (defparameter *app-menu*
   '(("0+INTERNET"
-     ("0-Firefox" firefox)
-     ("1-Chrome" google-chrome)
-     ("2-Teams" teams))
+     ("1-Firefox" firefox)
+     ("2-Chrome"  google-chrome)
+     ("3-Teams"   teams))
     ("1+VIRTUAL"
-     ("0-Virtualbox" "virtualbox")
-     ("1-Virt-Manager" "virt-manager"))
+     ("1-Virtualbox"   "virtualbox")
+     ("2-Virt-Manager" "virt-manager"))
     ("2+FUN"
-     ("0-Xlogo" "xlogo")
-     ("1-GnuChess" "xboard"))
+     ("1-Xlogo"    "xlogo")
+     ("2-GnuChess" "xboard"))
     ("3+WORK"
-     ("0-libreoffice" "libreoffice"))
+     ("1-libreoffice" "libreoffice"))
     ("4+GRAPHICS"
-     ("0-GIMP" "gimp"))
+     ("1-GIMP" "gimp"))
     ("5+VPN"
-     ("0-Connect VPN" "runvpn up")
-     ("1-Disconnect VPN" "runvpn down"))
+     ("1-Connect VPN"    "runvpn up")
+     ("2-Disconnect VPN" "runvpn down"))
     ("6*SYSTEM"
-     ("0+COMPOSITE"
-      ("0-On" "xcompmgr -C")
-      ("1-Off" "killall xcompmgr"))
+     ("1+COMPOSITE"
+      ("1-On"  "xcompmgr -C")
+      ("2-Off" "killall xcompmgr"))
      ("1+AUTOLOCK"
-      ("0-On" "xautolock -time 15 -locker slock")
-      ("1-Off" "killall xautolock"))))
+      ("1-On"  "xautolock -time 15 -locker slock")
+      ("2-Off" "killall xautolock"))))
   "Where the menu structure is held")
 
 (defun load-menu (file-name &key (strip 0))
@@ -66,5 +66,8 @@
                    (commandp (second selection)))
               (funcall (second selection)))
              (t
-              (pick (cdr selection)))))))
+              (if (equalp "0-.." (first selection))
+                  (pick (second selection))
+                  (pick (append (list (list "0-.." options))
+                                (cdr selection)))))))))
     (pick *app-menu*)))
