362ba05401aa508604604ffba1a139046eebe954
Zachary Zhou <zchrzhou@gmail.com>
2021-09-30 00:49:31 +0800

    more for font setting

--- stumpwmrc.d/font-setting.lisp
+++ stumpwmrc.d/font-setting.lisp
@@ -5,9 +5,10 @@
 
 (require 'clx-truetype)
 (require 'ttf-fonts)
-(or (and (xft:cache-fonts)
-         (set-font (make-instance 'xft:font
-                                  :family "WenQuanYi Micro Hei Mono"
-                                  :subfamily "Regular"
-                                  :size 13)))
-    (set-font "10x20"))
+(ignore-errors
+ (or (and (xft:cache-fonts)
+          (set-font (make-instance 'xft:font
+                                   :family "WenQuanYi Micro Hei Mono"
+                                   :subfamily "Regular"
+                                   :size 13)))
+     (set-font "10x20")))
