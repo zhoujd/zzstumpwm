0752180e0feac5feca65a6c9971eae371f6fa002
Zachary Zhou <zchrzhou@gmail.com>
2021-09-30 21:29:18 +0800

    clean code for env setting

--- stumpwmrc.d/env-setting.lisp
+++ stumpwmrc.d/env-setting.lisp
@@ -3,8 +3,5 @@
 
 (in-package :zwm)
 
-;; set DESKTOP_SESSION variable
-(setf (getenv "DESKTOP_SESSION") "stumpwm")
-
 ;; bugfix for scrolling doesn't work with an external mouse in GTK+3 apps.
 (setf (getenv "GDK_CORE_DEVICE_EVENTS") "1")
