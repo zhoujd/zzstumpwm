a86890d02c0300a4ac5c73174dd897aee3c4f33b
Zachary Zhou <zchrzhou@gmail.com>
2021-10-03 21:07:04 +0800

    more for ulauncher

--- libexec/ulauncher-setup.sh
+++ libexec/ulauncher-setup.sh
@@ -1,9 +1,11 @@
 #!/bin/bash
 
 ## https://ulauncher.io/
+## Press Alt+Enter to access an alt menu
 
 version=5.14.0
 Install_ulauncher() {
+    echo "Install ulauncher"
     local tmp=~/.tmp
     echo "Install ulauncher"
     mkdir -p $tmp
@@ -13,6 +15,16 @@ Install_ulauncher() {
     popd
 }
 
+Install_theme() {
+    echo "Install theme"
+    local theme_target=~/.config/ulauncher/user-themes
+    mkdir -p $theme_target
+    pushd $theme_target
+    git clone https://github.com/yerbestpal/ambiant-mate-ulauncher-theme
+    popd
+}
+
 Install_ulauncher
+Install_theme
 
 echo "Install ulauncher Done ..."
