437aa32bb4bb8f3e5f0ec6303682799c4199c29f
Zachary Zhou <zchrzhou@gmail.com>
2021-10-10 08:52:02 +0800

    more for sbcl setup

--- bin/sbcl-setup.sh
+++ bin/sbcl-setup.sh
@@ -1,30 +1,59 @@
 #!/bin/bash
 
-SBCL_VERSION="1.4.6"
+SBCL_VERSION="2.0.1"
 SBCL_PKG="sbcl-$SBCL_VERSION-x86-64-linux-binary.tar.bz2"
+SBCL_PREFIX=/usr/local
 
-echo "Download SBCL $SBCL_VERSION ...."
-
+## make sure the download folder exist
 mkdir -p ~/Downloads
 
-pushd ~/Downloads
+download() {
+    echo "Download SBCL $SBCL_VERSION ...."
+    pushd ~/Downloads
+    ## wget sbcl package
+    if [ ! -f $SBCL_PKG ]; then
+        wget http://prdownloads.sourceforge.net/sbcl/$SBCL_PKG
+    else
+        echo "The $SBCL_PKG already be downloaded"
+    fi
+    popd
+}
 
-## wget sbcl package
-if [ ! -f $SBCL_PKG ]; then
-    wget http://prdownloads.sourceforge.net/sbcl/$SBCL_PKG
-fi
+remove() {
+    echo "Remove old SBCL"
+    find /usr -name "sbcl*" | xargs sudo rm -rf
+}
 
-## untar package
-tar xf $SBCL_PKG
+install() {
+    echo "Install SBCL"
+    pushd ~/Downloads
+    if [ -f $SBCL_PKG ]; then
+        ## untar package
+        tar xf $SBCL_PKG
+        ## install sbcl
+        pushd sbcl-$SBCL_VERSION-x86-64-linux
+        sudo INSTALL_ROOT=$SBCL_PREFIX sh install.sh
+        popd
+    else
+        echo "Cannot find $SBCL_PKG, please download first"
+    fi
+    popd
+}
 
-## remove old sbcl
-#find /usr -name "sbcl*" | xargs sudo rm -rf
-
-## install sbcl
-pushd sbcl-$SBCL_VERSION-x86-64-linux
-sudo INSTALL_ROOT=/usr/local sh install.sh
-popd
-
-popd
+case $1 in
+    install )
+        download
+        install
+        ;;
+    download )
+        download
+        ;;
+    remove )
+        remove
+        ;;
+    * )
+        echo "Usage: $(basename $0) {install|download|remove}"
+        ;;
+esac
 
 echo "Install SBCL Done ..."
