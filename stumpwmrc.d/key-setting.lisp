0d52e6b9e0c04b677cb5b9fd79c9e4a92bfd413f
Zachary Zhou <zchrzhou@gmail.com>
2021-11-11 12:52:06 +0800

    refactoy number and fn keys

--- stumpwmrc.d/key-setting.lisp
+++ stumpwmrc.d/key-setting.lisp
@@ -7,18 +7,6 @@
 (apply-keys-to-map
  *zz-exec-map*
  (list
-  (kbd "1")                 "runemacs"
-  (kbd "2")                 "google-chrome"
-  (kbd "3")                 "teams"
-  (kbd "4")                 "evolution"
-  (kbd "5")                 "libreoffice"
-  (kbd "6")                 "exec libreoffice --writer"
-  (kbd "7")                 "exec libreoffice --calc"
-  (kbd "8")                 "exec libreoffice --impress"
-  (kbd "9")                 "trans-shell"
-  (kbd "0")                 "trans-command"
-  (kbd "-")                 "eval-command"
-  (kbd "=")                 "eval-shell"
   (kbd "a")                 "audacity"
   (kbd "C-a")               "exec audacity"
   (kbd "b")                 "google"
@@ -74,11 +62,6 @@
 (apply-keys-to-map
  *zz-x-map*
  (list
-  (kbd "1")                 "only"
-  (kbd "2")                 "vsplit"
-  (kbd "3")                 "hsplit"
-  (kbd "4")                 "kill"
-  (kbd "0")                 "remove"
   (kbd "c")                 "fclear"
   (kbd "d")                 "delete"
   (kbd "f")                 "toggle-float"
@@ -221,19 +204,6 @@
 (apply-keys-to-map
  *zz-screen-map*
  (list
-  (kbd "1")                 "pactl-toggle"
-  (kbd "2")                 "pactl-down"
-  (kbd "3")                 "pactl-up"
-  (kbd "4")                 "pactl-mic-toggle"
-  (kbd "5")                 "exec xdotool key F5"
-  (kbd "6")                 "touchpad-toggle"
-  (kbd "7")                 "wifi-toggle"
-  (kbd "8")                 "cheese"
-  (kbd "9")                 "exec zlock"
-  (kbd "0")                 "screen-menu"
-  (kbd "-")                 "bright-down"
-  (kbd "=")                 "bright-up"
-
   (kbd "d")                 "toggle-root"
   (kbd "f")                 "fullscreen"
   (kbd "g")                 "refresh"
@@ -277,12 +247,6 @@
 (apply-keys-to-map
  *zz-c-map*
  (list
-  (kbd "1")                 (rat-click 1)   ;;left
-  (kbd "2")                 (rat-click 2)   ;;middle
-  (kbd "3")                 (rat-click 3)   ;;right
-  (kbd "4")                 (rat-click 4)   ;;wheel up
-  (kbd "5")                 (rat-click 5)   ;;whell down
-
   (kbd "b")                 (getkey-top (kbd "s-Left"))
   (kbd "f")                 (getkey-top (kbd "s-Right"))
   (kbd "p")                 (getkey-top (kbd "s-Up"))
---
+++ stumpwmrc.d/nkey-setting.lisp
@@ -0,0 +1,58 @@
+;;;; Number 1 to 12 setting -*-lisp-*-
+;;
+
+(in-package :zwm)
+
+(apply-keys-to-map
+ *zz-exec-map*
+ (list
+  (kbd "1")                 "runemacs"
+  (kbd "2")                 "google-chrome"
+  (kbd "3")                 "teams"
+  (kbd "4")                 "evolution"
+  (kbd "5")                 "libreoffice"
+  (kbd "6")                 "exec libreoffice --writer"
+  (kbd "7")                 "exec libreoffice --calc"
+  (kbd "8")                 "exec libreoffice --impress"
+  (kbd "9")                 "trans-shell"
+  (kbd "0")                 "trans-command"
+  (kbd "-")                 "eval-command"
+  (kbd "=")                 "eval-shell"
+  ))
+
+(apply-keys-to-map
+ *zz-x-map*
+ (list
+  (kbd "1")                 "only"
+  (kbd "2")                 "vsplit"
+  (kbd "3")                 "hsplit"
+  (kbd "4")                 "kill"
+  (kbd "0")                 "remove"
+  ))
+
+(apply-keys-to-map
+ *zz-screen-map*
+ (list
+  (kbd "1")                 "pactl-toggle"
+  (kbd "2")                 "pactl-down"
+  (kbd "3")                 "pactl-up"
+  (kbd "4")                 "pactl-mic-toggle"
+  (kbd "5")                 "exec xdotool key F5"
+  (kbd "6")                 "touchpad-toggle"
+  (kbd "7")                 "wifi-toggle"
+  (kbd "8")                 "cheese"
+  (kbd "9")                 "exec zlock"
+  (kbd "0")                 "screen-menu"
+  (kbd "-")                 "bright-down"
+  (kbd "=")                 "bright-up"
+  ))
+
+(apply-keys-to-map
+ *zz-c-map*
+ (list
+  (kbd "1")                 (rat-click 1)   ;;left
+  (kbd "2")                 (rat-click 2)   ;;middle
+  (kbd "3")                 (rat-click 3)   ;;right
+  (kbd "4")                 (rat-click 4)   ;;wheel up
+  (kbd "5")                 (rat-click 5)   ;;whell down
+  ))
