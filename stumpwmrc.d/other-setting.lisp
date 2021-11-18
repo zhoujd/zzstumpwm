5d537aa1f96d564e6f5923ff5c74654fcae216fa
Zachary Zhou <zchrzhou@gmail.com>
2021-11-18 09:47:14 +0800

    add more for message

--- stumpwmrc.d/other-setting.lisp
+++ stumpwmrc.d/other-setting.lisp
@@ -80,6 +80,13 @@ run-or-raise with group search t."
         (run-or-raise cmd `(:class ,win-cls) nil T)
         (run-or-raise cmd `(:class ,win-cls) T T))))
 
+(defun gravity-message (location string)
+  "gravity message, A gravity symbol. One of :center :top :right :bottom :left
+     :top-right :top-left :bottom-right :bottom-left"
+  (let ((old-location *message-window-gravity*))
+    (setf *message-window-gravity* location)
+    (echo string)
+    (setf *message-window-gravity* old-location)))
 
 ;; startup run commands
 (mapc
