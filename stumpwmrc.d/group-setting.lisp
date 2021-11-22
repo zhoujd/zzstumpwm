3852a0489bd2aa850fd30b938412d39f0b47adde
Zachary Zhou <zchrzhou@gmail.com>
2021-11-22 20:49:30 +0800

    more for group arrow keys

--- stumpwmrc.d/group-setting.lisp
+++ stumpwmrc.d/group-setting.lisp
@@ -33,3 +33,75 @@
   (if (typep (current-group) 'stumpwm::tile-group)
       (run-commands "pull-hidden-previous")
       (run-commands "float-window-previous")))
+
+(defcommand move-focus-left () ()
+  "move focus left"
+  (if (typep (current-group) 'stumpwm::tile-group)
+      (run-commands "move-focus left")
+      (run-commands "move-float-window -10 0")))
+
+(defcommand move-focus-right () ()
+  "move focus right"
+  (if (typep (current-group) 'stumpwm::tile-group)
+      (run-commands "move-focus right")
+      (run-commands "move-float-window +10 0")))
+
+(defcommand move-focus-up () ()
+  "move focus up"
+  (if (typep (current-group) 'stumpwm::tile-group)
+      (run-commands "move-focus up")
+      (run-commands "move-float-window 0 -10")))
+
+(defcommand move-focus-down () ()
+  "move focus up"
+  (if (typep (current-group) 'stumpwm::tile-group)
+      (run-commands "move-focus down")
+      (run-commands "move-float-window 0 +10")))
+
+(defcommand move-window-left () ()
+  "move window left"
+  (if (typep (current-group) 'stumpwm::tile-group)
+      (run-commands "move-window left")
+      (run-commands "resize-float-window -10 0")))
+
+(defcommand move-window-right () ()
+  "move window right"
+  (if (typep (current-group) 'stumpwm::tile-group)
+      (run-commands "move-window right")
+      (run-commands "resize-float-window +10 0")))
+
+(defcommand move-window-up () ()
+  "move window up"
+  (if (typep (current-group) 'stumpwm::tile-group)
+      (run-commands "move-window up")
+      (run-commands "resize-float-window 0 -10")))
+
+(defcommand move-window-down () ()
+  "move window up"
+  (if (typep (current-group) 'stumpwm::tile-group)
+      (run-commands "move-window down")
+      (run-commands "resize-float-window 0 +10")))
+
+(defcommand exchange-left () ()
+  "exchange left"
+  (if (typep (current-group) 'stumpwm::tile-group)
+      (run-commands "exchange-direction left")
+      (run-commands "float-window-gravity left")))
+
+(defcommand exchange-right () ()
+  "exchange right"
+  (if (typep (current-group) 'stumpwm::tile-group)
+      (run-commands "exchange-direction right")
+      (run-commands "float-window-gravity right")))
+
+(defcommand exchange-up () ()
+  "exchange up"
+  (if (typep (current-group) 'stumpwm::tile-group)
+      (run-commands "exchange-direction up")
+      (run-commands "float-window-gravity top")))
+
+(defcommand exchange-down () ()
+  "exchange up"
+  (if (typep (current-group) 'stumpwm::tile-group)
+      (run-commands "exchange-direction down")
+      (run-commands "float-window-gravity bottom")))
