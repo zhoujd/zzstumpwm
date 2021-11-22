21f4bcb6f73a56bd0e698dfb759310228148bdee
Zachary Zhou <zchrzhou@gmail.com>
2021-11-22 13:39:19 +0800

    clean code

--- stumpwmrc.d/command-setting.lisp
+++ stumpwmrc.d/command-setting.lisp
@@ -429,33 +429,28 @@ used for matching windows with run-or-raise or window placement-merules."
   "Focus previously focused floating window."
   (if (typep (current-group) 'stumpwm::tile-group)
       (run-commands "pull-hidden-other")
-      (run-commands "float-window-other")
-      ))
+      (run-commands "float-window-other")))
 
 (defcommand window-next () ()
   "Focus next window."
   (if (typep (current-group) 'stumpwm::tile-group)
       (run-commands "next-in-frame")
-      (run-commands "float-window-next")
-      ))
+      (run-commands "float-window-next")))
 
 (defcommand window-previous () ()
   "Focus previous window."
   (if (typep (current-group) 'stumpwm::tile-group)
       (run-commands "prev-in-frame")
-      (run-commands "float-window-previous")
-      ))
+      (run-commands "float-window-previous")))
 
 (defcommand pull-window-next () ()
   "Pull next window."
   (if (typep (current-group) 'stumpwm::tile-group)
       (run-commands "pull-hidden-next")
-      (run-commands "float-window-next")
-      ))
+      (run-commands "float-window-next")))
 
 (defcommand pull-window-prev () ()
   "Pull previous window."
   (if (typep (current-group) 'stumpwm::tile-group)
       (run-commands "pull-hidden-previous")
-      (run-commands "float-window-previous")
-      ))
+      (run-commands "float-window-previous")))
