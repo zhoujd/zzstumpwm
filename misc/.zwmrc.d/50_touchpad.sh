d49282a6b45a992687628142c4697ad2645e4ecd
Zachary Zhou <zchrzhou@gmail.com>
2021-11-30 18:58:18 +0800

    more for clean on touchpad

--- misc/.zwmrc.d/50_touchpad.sh
+++ misc/.zwmrc.d/50_touchpad.sh
@@ -4,8 +4,6 @@ touchpad() {
     if [ -n "$(xinput list | grep -i touchpad)" ]; then
         echo "[touchpad] disable tap to click"
         synclient TapButton1=0
-        echo "[touchpad] disable touchpad"
-        synclient TouchpadOff=1
     fi           
 }
 
