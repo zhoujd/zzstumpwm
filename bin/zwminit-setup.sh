73eaa48fbedf72a9e61aa5e827c16d2deb622c69
Zachary Zhou <zchrzhou@gmail.com>
2021-12-09 08:08:34 +0800

    more for zwm init

--- bin/zwminit-setup.sh
+++ bin/zwminit-setup.sh
@@ -5,9 +5,12 @@ SCRIPT_ROOT=$(cd $(dirname "${BASH_SOURCE[0]}") && pwd)
 ZZSTUMPWM_ROOT=$(cd $SCRIPT_ROOT/.. && pwd)
 
 install_zwmrc() {
-    local target=/usr/bin/zwmrc
     ln -sfvT $ZZSTUMPWM_ROOT/misc/.zwmrc ~/.zwmrc
     ln -sfvT $ZZSTUMPWM_ROOT/misc/.zwmrc.d ~/.zwmrc.d
+}
+
+install_zwminit() {
+    local target=/usr/bin/zwminit
     sudo tee $target <<EOF
 #!/bin/sh
 for i in ~/.zwmrc.d/*.sh ; do
@@ -18,5 +21,6 @@ EOF
 }
 
 install_zwmrc
+install_zwminit
 
-echo "zwmrc setup done"
+echo "zwminit setup done"
