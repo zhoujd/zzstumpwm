c3d7423a91775eca2162a77e5902b479f7ca6d25
Zachary Zhou <zchrzhou@gmail.com>
2021-12-17 14:31:29 +0800

    more for stumpwm dm setup

--- bin/stumpwm-dm-setup.sh
+++ bin/stumpwm-dm-setup.sh
@@ -1,16 +1,26 @@
 #!/bin/bash
 
+SCRIPT_ROOT=$(cd $(dirname "${BASH_SOURCE[0]}") && pwd)
+TARGET=/usr/bin
+
 ## Please run as root
 if [ $EUID -ne 0 ]; then
     echo "You must be a root user" 2>&1
     exit 1
 fi
 
-echo "[Desktop Entry]
+echo "Setup stumpwm-session"
+ln -sfvT $SCRIPT_ROOT/stumpwm-session $TARGET/stumpwm-session
+
+echo "Setup stumpwm desktop"
+tee /usr/share/xsessions/stumpwm.desktop <<EOF
+[Desktop Entry]
 Name=Stumpwm
 Comment=Stumpwm window manager
 Exec=stumpwm-session
 Icon=
 Type=Application
 DesktopNames=STUMPWM
-" > /usr/share/xsessions/stumpwm.desktop
+EOF
+
+echo "Setup stumpwm dm done"
