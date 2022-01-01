75d6f9c445d58ae8cec506d37447acd1856ce27c
Zachary Zhou <zchrzhou@gmail.com>
2022-01-01 09:13:18 +0800

    more for input method

---
+++ misc/.zwmrc.d/30_inputmethod.sh
@@ -0,0 +1,11 @@
+#!/bin/bash
+
+im_ibus() {
+    echo "[inputmethod] ibus as input method"
+    export LANG=en_US.UTF-8
+    export GTK_IM_MODULE=ibus
+    export QT_IM_MODULE=ibus
+    export XMODIFIERS=@im=ibus
+}
+
+im_ibus
