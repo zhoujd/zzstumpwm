3218fae57468dc10489150241d3dea92c877b155
Zachary Zhou <zchrzhou@gmail.com>
2022-02-02 20:52:59 +0800

    more for slop

--- libexec/stumpwm-tool.sh
+++ libexec/stumpwm-tool.sh
@@ -6,12 +6,12 @@ case $target in
     ubuntu )
         sudo apt install -y rlwrap
         sudo apt install -y sbcl
-        sudo apt install -y rofi keynav
+        sudo apt install -y rofi keynav slop
         ;;
     arch|majaro )
         sudo pacman -S rlwrap
         sudo pacman -S sbcl
-        sudo pacman -S rofi keynav
+        sudo pacman -S rofi keynav slop
         ;;
     * )
         echo "Use $0 [ubuntu|arch|manjaro]"
