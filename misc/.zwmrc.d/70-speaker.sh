25c8e3bc262ad49c2d71b1d3b26056e43b04e6e3
Zachary Zhou <zchrzhou@gmail.com>
2022-03-05 20:52:11 +0800

    more for speaker

--- misc/.zwmrc.d/70-speaker.sh
+++ misc/.zwmrc.d/70-speaker.sh
@@ -2,7 +2,7 @@
 
 speaker_init() {
     local defvol=0
-    local speaker=$(pactl list short sinks | cut -f 2 | grep pci-0000)
+    local speaker=$(pactl list short sinks | grep pci-0000 | cut -f 1)
     if [ -n $speaker ]; then
         echo "[speaker] set volume to $defvol and mute"
         pactl set-sink-volume $speaker $defvol
