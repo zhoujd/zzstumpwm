38904e007df7fef12c7630c7b99400737380974c
Zachary Zhou <zchrzhou@gmail.com>
2022-03-05 17:07:59 +0800

    more for build-in speaker

--- misc/.zwmrc.d/70-speaker.sh
+++ misc/.zwmrc.d/70-speaker.sh
@@ -4,8 +4,9 @@ speaker_init() {
     local defvol=0
     local speaker=$(pactl list short sinks | cut -f 2 | grep pci-0000)
     if [ -n $speaker ]; then
-        echo "[speaker] set volume to $defvol"
+        echo "[speaker] set volume to $defvol and mute"
         pactl set-sink-volume $speaker $defvol
+        pactl set-sink-mute $speaker off
     fi
 }
 
