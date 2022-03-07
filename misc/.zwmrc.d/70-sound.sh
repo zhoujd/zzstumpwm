10a652a4055ff17ac70447516a7bbb26219dce32
Zachary Zhou <zchrzhou@gmail.com>
2022-03-07 09:34:35 +0800

    rename script

--- misc/.zwmrc.d/70-sound.sh
+++ misc/.zwmrc.d/70-sound.sh
@@ -4,7 +4,7 @@ speaker_init() {
     local defvol=0
     local speaker=$(pactl list short sinks | grep pci-0000 | cut -f 1)
     if [ -n $speaker ]; then
-        echo "[speaker] set volume to $defvol and mute"
+        echo "[sound] set speaker volume to $defvol and mute"
         pactl set-sink-volume $speaker $defvol
         pactl set-sink-mute $speaker off
     fi
@@ -13,7 +13,7 @@ speaker_init() {
 headset_init() {
     local headset=$(pactl list short sinks | grep usb | cut -f 2)
     if [ -n $headset ]; then
-        echo "[speaker] set usb headset as default sink"
+        echo "[sound] set usb headset as default sink"
         pactl set-default-sink $headset
     fi
 }
