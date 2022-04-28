beb0dd268365743797e9437477ce544bc59b26c0
Zachary Zhou <zchrzhou@gmail.com>
2022-04-28 14:42:17 +0800

    more for nnn

--- libexec/nnn-setup.sh
+++ libexec/nnn-setup.sh
@@ -29,7 +29,7 @@ config() {
     sudo tee /etc/profile.d/zz-nnn.sh <<EOF
 ### zz-nnn.sh
 
-export NNN_BMS='d:~/Downloads/;w:~/work/;/:/;z:/zach/'
+export NNN_BMS='d:~/Downloads/;w:~/work/;/:/;~:~/;z:/zach/'
 export NNN_SSHFS='sshfs -o follow_symlinks'
 export NNN_TRASH=1
 export NNN_FIFO='/tmp/nnn.fifo'
