514a8a97e50fd994782a947cd933ab3e7b62cc78
Zachary Zhou <zchrzhou@gmail.com>
2022-05-12 19:38:56 +0800

    clean code

--- stumpwmrc.d/tool-setting.lisp
+++ stumpwmrc.d/tool-setting.lisp
@@ -5,22 +5,27 @@
 
 ;; auto start swank server
 (require 'swank)
-(progn
-  (defvar *swank-server-running* nil
-    "swank server flag")
-  (defvar *swank-server-port* 4405
-    "swank server port")
-  (defun swank-start-server ()
-    (and (not *swank-server-running*)
-         (swank:create-server :port *swank-server-port*
-                              :dont-close t)
-         (setf *swank-server-running* t)))
-  (defun swank-stop-server ()
-    (and *swank-server-running*
-         (swank:stop-server *swank-server-port*)
-         (setf *swank-server-running* nil)))
-  ;; restart server
-  (swank-start-server))
+(defvar *swank-server-running* nil
+  "swank server flag")
+(defvar *swank-server-port* 4405
+  "swank server port")
+(defun swank-start-server ()
+  (when (not *swank-server-running*)
+    (swank:create-server :port *swank-server-port*
+                         :dont-close t)
+    (setf *swank-server-running* t)))
+(defun swank-stop-server ()
+  (when *swank-server-running*
+    (swank:stop-server *swank-server-port*)
+    (setf *swank-server-running* nil)))
+(defun swank-restart-server ()
+  (swank:stop-server *swank-server-port*)
+  (swank:create-server :port *swank-server-port*
+                       :dont-close t)
+  (setf *swank-server-running* t))
+
+;; restart server
+(swank-restart-server)
 
 ;; screen shot
 (require 'screenshot)
