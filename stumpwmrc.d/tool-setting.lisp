3c1d7718120c19a38cd3a2ba4a830ce249db937e
Zachary Zhou <zchrzhou@gmail.com>
2022-05-12 19:52:29 +0800

    format code

--- stumpwmrc.d/tool-setting.lisp
+++ stumpwmrc.d/tool-setting.lisp
@@ -5,24 +5,33 @@
 
 ;; auto start swank server
 (require 'swank)
+
 (defvar *swank-server-running* nil
   "swank server flag")
+
 (defvar *swank-server-port* 4405
   "swank server port")
+
 (defun swank-start-server ()
+  "swank start server"
   (when (not *swank-server-running*)
     (swank:create-server :port *swank-server-port*
                          :dont-close t)
     (setf *swank-server-running* t)))
+
 (defun swank-stop-server ()
+  "swank stop server"
   (when *swank-server-running*
     (swank:stop-server *swank-server-port*)
     (setf *swank-server-running* nil)))
+
 (defun swank-restart-server ()
-  (swank:stop-server *swank-server-port*)
-  (swank:create-server :port *swank-server-port*
-                       :dont-close t)
-  (setf *swank-server-running* t))
+  "swank restart server"
+  (ignore-errors
+   (swank:stop-server *swank-server-port*)
+   (swank:create-server :port *swank-server-port*
+                        :dont-close t)
+   (setf *swank-server-running* t)))
 
 ;; restart server
 (swank-restart-server)
