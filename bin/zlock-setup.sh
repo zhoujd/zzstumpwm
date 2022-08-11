52e20c16bb57caf604cf76f4c508f2cf4a170b00
Zachary Zhou <zchrzhou@gmail.com>
2022-08-11 13:04:43 +0800

    remove lock icon

--- bin/zlock-setup.sh
+++ bin/zlock-setup.sh
@@ -6,8 +6,4 @@ ZZSTUMPWM_ROOT=$(cd $SCRIPT_ROOT/.. && pwd)
 BIN_TARGET=/usr/bin
 sudo ln -sfvT $ZZSTUMPWM_ROOT/bin/zlock $BIN_TARGET/zlock
 
-ICON_TARGET=~/.icons
-mkdir -p $ICON_TARGET
-cp -vf $ZZSTUMPWM_ROOT/misc/.icons/lock.png $ICON_TARGET
-
 echo "setup zlock done"
