c1872fbecd7c16acf62094e6a7b7f231bf6c867f
Zachary Zhou <zchrzhou@gmail.com>
2022-08-15 13:06:31 +0800

    more for -z

--- misc/.zwmrc.d/70-sound.sh
+++ misc/.zwmrc.d/70-sound.sh
@@ -3,8 +3,8 @@
 speaker_init() {
     local defvol=0
     local speaker=$(pactl list short sinks | grep pci-0000 | cut -f 1)
-    if [ -n $speaker ]; then
-        echo "[sound] set speaker volume to $defvol and mute"
+    if [ -n "$speaker" ]; then
+        echo "[sound] set speaker $speaker volume to $defvol and mute"
         pactl set-sink-volume $speaker $defvol
         pactl set-sink-mute $speaker off
     fi
@@ -13,8 +13,8 @@ speaker_init() {
 headset_init() {
     local headset=$(pactl list short sinks | grep usb | cut -f 2)
     local defvol=50%
-    if [ -n $headset ]; then
-        echo "[sound] set usb headset as default sink"
+    if [ -n "$headset" ]; then
+        echo "[sound] set usb headset $headset as default sink"
         pactl set-default-sink $headset
         pactl set-sink-volume $headset $defvol
     fi
