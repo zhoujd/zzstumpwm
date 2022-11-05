ab27b9125554f186a6e4dd8a0f7a6d2c59a639fa
Zachary Zhou <zchrzhou@gmail.com>
2022-11-05 23:44:46 +0800

    more for backlight

--- libexec/backlight-setup.sh
+++ libexec/backlight-setup.sh
@@ -1,50 +1,25 @@
 #!/bin/bash
 #set -x -v
 
+## https://wiki.archlinux.org/title/backlight
+
 SCRIPT_ROOT=$(cd $(dirname "${BASH_SOURCE[0]}") && pwd)
 ZZSTUMPWM_ROOT=$(cd $SCRIPT_ROOT/.. && pwd)
 BRIGHT_PATH=/usr/bin/brightness-setup.sh
+UDEV_RULES=/etc/udev/rules.d/90-backlight.rules
 
 install_brightness() {
+    echo "Install backlight udev rules"
     sudo ln -sfv $ZZSTUMPWM_ROOT/libexec/brightness-setup.sh $BRIGHT_PATH
 }
 
 install_auto() {
-    local backlight_rc=z02backlight
-    if [ -d /etc/rc.zach.d ]; then
-        sudo tee /etc/rc.zach.d/$backlight_rc <<EOF
-#!/bin/bash
-
-do_start() {
-    echo "\$0 start"
-    test -x $BRIGHT_PATH && $BRIGHT_PATH
-}
-
-do_stop() {
-    echo "\$0 stop"
-}
-
-case "\$1" in
-    start)
-    	do_start
-        ;;
-    stop)
-        do_stop
-        ;;
-    *)
-        echo "Usage: \$0 {start|stop}" >&2
-        exit 3
-        ;;
-esac
-exit 0
+    echo "Install backlight $UDEV_RULES"
+    sudo tee $UDEV_RULES <<EOF
+ACTION=="add", SUBSYSTEM=="backlight", RUN+="/bin/chgrp video $sys$devpath/brightness", RUN+="/bin/chmod g+w $sys$devpath/brightness"
 EOF
-        sudo chmod +x /etc/rc.zach.d/$backlight_rc
-    else
-        echo "Install /etc/rc.zach.d is not exist"
-    fi
-    
+    echo "Please reboot"
     echo "Install auto done"
-
 }
 
 install_brightness
