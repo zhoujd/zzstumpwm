2a367cb16de364d06c147ba56788bb1ae26bb27e
Zachary Zhou <zchrzhou@gmail.com>
2022-11-11 22:42:49 +0800

    more for asdf setup

--- libexec/asdf-setup.sh
+++ libexec/asdf-setup.sh
@@ -5,8 +5,8 @@ cfg_setup() {
     local tgt=$HOME/.config/common-lisp/source-registry.conf.d
     mkdir -p $tgt
     tee $tgt/50-${USER}-lisp.conf <<EOF
-(:tree (:home "common-lisp"))
-(:tree (:home "lisp"))
+(:tree "$HOME/common-lisp")
+(:tree "$HOME/lisp")
 EOF
 }
 
