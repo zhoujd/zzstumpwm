63c67f1178777c9f40c96068f90a520d2365e8ad
Zachary Zhou <zchrzhou@gmail.com>
2022-11-24 07:34:39 +0800

    more for display on/off

--- libexec/display-off.sh
+++ libexec/display-off.sh
@@ -2,7 +2,7 @@
 
 echo "Usage: $(basename $0) {DP}
 DP     defaut: eDP-1
-Note: run 'xrand' for monitor information"
+Note: run 'xrandr' for monitor information"
 
 DP=${1:-eDP-1}
 xrandr --output $DP --off
