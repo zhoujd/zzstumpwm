af7600015826c5e1ddc08e1d22130e124137669c
Zachary Zhou <zchrzhou@gmail.com>
2022-12-25 20:42:55 +0800

    more for env gtk

--- stumpwmrc.d/env-setting.lisp
+++ stumpwmrc.d/env-setting.lisp
@@ -3,5 +3,5 @@
 
 (in-package :zwm)
 
-;; bugfix for scrolling doesn't work with an external mouse in GTK+3 apps.
+;; external mouse scrolling in GTK+3 apps.
 (setf (getenv "GDK_CORE_DEVICE_EVENTS") "1")
