e04a351b08b526afda1e371f3aeda446a9bd28a6
Zachary Zhou <zchrzhou@gmail.com>
2023-08-15 10:43:54 +0800

    Format code

--- libexec/redshift-setup.sh
+++ libexec/redshift-setup.sh
@@ -7,39 +7,45 @@ ZZSTUMPWM_ROOT=$(cd $SCRIPT_ROOT/.. && pwd)
 ## $ sudo apt install intltool
 ## $ ./bootstrap
 ## $ automake --add-missing --copy --force-missing
-## $ ./configure
+## $ ./configure --prefix=/usr
 ## $ make
 ## $ sudo make install
 
-Install_redshift() {
+Install_deps() {
     echo "Install redshift packages"
     sudo apt install -y redshift
 }
 
 Install_cfg() {
-    echo "Install redshift config"
     cp -fv $ZZSTUMPWM_ROOT/misc/.config/redshift.conf ~/.config/redshift.conf
+    echo "Install redshift config done"
+}
 
+Install_service() {
     if [ -e /usr/bin/redshift ]; then
         ln -sfTv $ZZSTUMPWM_ROOT/misc/.config/systemd/user/redshift.service ~/.config/systemd/user/redshift.service
         systemctl daemon-reload --user
         systemctl --user enable --now redshift
         systemctl --user start --now redshift
         systemctl --user status redshift
-        echo "redshift setup done"
+        echo "Install redshift service done"
     else
         echo "No redshift are found"
     fi
 }
 
 case $1 in
-    install )
-        Install_redshift
+    deps )
+        Install_deps
         ;;
     service )
         Install_cfg
+        Install_service
+        ;;
+    status )
+        systemctl --user  status redshift
         ;;
     * )
-        echo "Usage: $(basename $0) {install|service}"
+        echo "Usage: $(basename $0) {deps|service|status}"
         ;;
 esac
