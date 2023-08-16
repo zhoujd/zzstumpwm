f75bd896d35396ec84190c2dcca3818d4eb567af
Zachary Zhou <zchrzhou@gmail.com>
2023-08-16 10:09:20 +0800

    Update for redshift service

--- misc/.zwmrc.d/80-system.sh
+++ misc/.zwmrc.d/80-system.sh
@@ -1,4 +1,6 @@
 #!/bin/bash
 
-systemctl restart --user redshift.service
-echo "[system] restart redshift service"
+if [ -n "$(systemctl list-units --full -all --user | grep redshift.service)" ]; then
+    systemctl restart --user redshift.service
+    echo "[system] restart redshift service"
+fi
