64641e487ea738f32f84c85d81f2d3bbaf512bb9
Zachary Zhou <zchrzhou@gmail.com>
2023-08-16 09:56:13 +0800

    More for screen update

--- misc/.zwmrc.d/60-screen.sh
+++ misc/.zwmrc.d/60-screen.sh
@@ -1,11 +1,13 @@
 #!/bin/bash
+#set -x
 
 screen() {
     local laptop="eDP-1"
     local ext="DP-1"
-    local flag=`xrandr | grep "^$ext" | awk '{print $2}'`
+    local laptop_flag=$(xrandr | grep "^$laptop" | awk '{print $2}')
+    local ext_flag=$(xrandr | grep "^$ext" | awk '{print $2}')
     local size="1920x1080"
-    if [ "$flag" = "connected" ]; then
+    if [ "$ext_flag" = "connected" -a "$laptop_flag" = "connected" ]; then
         echo "[screen] mirror ext monitor"
         xrandr --addmode $ext $size
         xrandr --output $ext --same-as $laptop --mode $size
