c5e58ec2c5bc2dd4d47556c18a522b6b9cc8c1ca
Zachary Zhou <zchrzhou@gmail.com>
2023-09-12 09:53:21 +0800

    Add more for font comments

--- stumpwmrc.d/font-setting.lisp
+++ stumpwmrc.d/font-setting.lisp
@@ -5,6 +5,8 @@
 
 ;;find a font of your choice in `xfontsel' and put it here.
 ;(set-font "*-unifont-medium-*-normal-*-16-*-*-*-*-*-*-*")
+;(setf xft:*font-dirs* '("/run/current-system/profile/share/fonts/"))
+;(setf clx-truetype:+font-cache-filename+ (concat (getenv "HOME") "/.fonts/font-cache.sexp"))
 
 #-clx-truetype
 (require 'clx-truetype)
