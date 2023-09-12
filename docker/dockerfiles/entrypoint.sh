13a6d4397fa9e9fa42bf6760b042a9e25456f3e5
Zachary Zhou <zchrzhou@gmail.com>
2023-09-12 07:11:46 +0000

    Add docker files

---
+++ docker/dockerfiles/entrypoint.sh
@@ -0,0 +1,74 @@
+#!/bin/bash
+
+SCRIPT_ROOT=$(cd $(dirname "${BASH_SOURCE[0]}") && pwd)
+ZZEMACS_ROOT=$HOME/zzemacs
+ZWM_ROOT=$HOME/zzstumpwm
+
+setup_common() {
+    echo "Setup common ..."
+    touch $HOME/.Xauthority
+}
+
+setup_zzemacs() {
+    local install_cmd=$ZZEMACS_ROOT/docker/script/install.sh
+    if [ -x $install_cmd ]; then
+        echo "Setup zzemacs ..."
+        $install_cmd
+    fi
+}
+
+setup_libvirtd() {
+    local libvirtd_cmd=/usr/sbin/libvirtd
+    if [ -x $libvirtd_cmd ]; then
+        echo "Setup libvirtd ..."
+        sudo $libvirtd_cmd -d
+    fi
+}
+
+setup_dbus() {
+    local dbus_cmd=/etc/init.d/dbus
+    if [ -x $dbus_cmd ]; then
+        echo "Setup dbus ..."
+        sudo $dbus_cmd start
+    fi
+}
+
+setup_ssh() {
+    local ssh_cmd=/etc/init.d/ssh
+    if [ -x $ssh_cmd ]; then
+        echo "Setup ssh ..."
+        sudo $ssh_cmd start
+    fi
+}
+
+setup_zwm() {
+    echo "Setup zwm ..."
+    pushd $ZWM_ROOT
+    ./install.sh
+    sleep 2s
+    ./bin/zwm-session
+    popd
+}
+
+setup_help() {
+    echo "Usage: $0 {init|help}"
+}
+
+
+CMD=${1:-""}
+case "$CMD" in
+    "init" )
+        setup_common
+        setup_zzemacs
+        setup_libvirtd
+        setup_dbus
+        setup_ssh
+        setup_zwm
+        ;;
+    "help" )
+        setup_help
+        ;;
+    * )
+        exec "$@"
+        ;;
+esac
---
+++ docker/run.sh
@@ -0,0 +1,91 @@
+#!/bin/bash
+#set -x
+
+SCRIPT_ROOT=$(cd $(dirname "${BASH_SOURCE[0]}") && pwd)
+ZWM_ROOT=$(cd $SCRIPT_ROOT/.. && pwd)
+ZWM_TOP=$(cd $ZWM_ROOT/.. && pwd)
+
+IMG=${IMG:-zz/ubuntu-20.04-zwm}
+TAG=${TAG:-base}
+CTN_PREFIX=${CTN_PREFIX:-zwm}
+CTN_NAME=${CTN_NAME:-$CTN_PREFIX-$TAG}
+CTN_HOST=${CTN_HOST:-ubuntu-2004-zwm}
+CTN_USER=${CTN_USER:-$USER}
+CTN_HOME=${CTN_HOME:-/home/$CTN_USER}
+SSH_HOST=${SSH_HOST:-localhost}
+SSH_PORT=${SSH_PORT:-9022}
+SSH_USER=${CTN_USER}
+HOST_NAME=${HOST_NAME:-dockerhost}
+HOST_IP=${HOST_IP:-host-gateway}
+PROMPT=${PROMPT:-$(basename $0)}
+
+## Use local X11 Server
+X11_PARAM=(
+    -e DISPLAY=$DISPLAY
+    -v /tmp/.X11-unix:/tmp/.X11-unix
+)
+
+RUN_PARAM=(
+    -it
+    --rm
+    -e DISPLAY=:100
+    -e SHELL=/bin/bash
+    --privileged=true
+    --cap-add=ALL
+    --add-host=$HOST_NAME:$HOST_IP
+    -h $CTN_HOST
+    -u $CTN_USER
+    -p $SSH_PORT:22
+    -v /dev:/dev
+    -v /tmp/.X11-unix:/tmp/.X11-unix
+    -v /var/run/docker.sock:/var/run/docker.sock
+    -v /etc/security/limits.conf:/etc/security/limits.conf
+    -v /etc/sysctl.conf:/etc/sysctl.conf
+    -v $ZWM_ROOT:$CTN_HOME/zzstumpwm
+    -v $ZWM_TOP/zzemacs:$CTN_HOME/zzemacs
+    -v $ZWM_TOP/lab:$CTN_HOME/lab
+)
+
+EXEC_PARAM=(
+    -e DISPLAY=$DISPLAY
+    -e SHELL=/bin/bash
+    -u $CTN_USER
+)
+
+EMACS_PARAM=(
+    emacs
+    -nw
+)
+
+SHELL_PARAM=(
+    bash
+    -l
+)
+
+case $1 in
+    start )
+        docker run --name=${CTN_NAME} ${RUN_PARAM[@]} ${IMG}:${TAG}
+        ;;
+    stop )
+        docker stop ${CTN_NAME} && docker rm ${CTN_NAME}
+        ;;
+    status )
+        docker ps | grep ${CTN_NAME}
+        ;;
+    emacs )
+        docker exec -it ${EXEC_PARAM[@]} ${CTN_NAME} ${EMACS_PARAM[@]}
+        ;;
+    shell )
+        docker exec -it ${EXEC_PARAM[@]} ${CTN_NAME} ${SHELL_PARAM[@]}
+        ;;
+    ssh )
+        TERM=xterm ssh -X -l ${SSH_USER} ${SSH_HOST} -p ${SSH_PORT}
+        ;;
+    build )
+        shift
+        make -C dockerfiles $@
+        ;;
+    * )
+        echo "Usage: ${PROMPT} {start|stop|status|emacs|shell|ssh|build}"
+        ;;
+esac
