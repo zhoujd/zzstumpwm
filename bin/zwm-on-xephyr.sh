02af903bcf9c42e86dc5515d53c78eab21482030
Zachary Zhou <zchrzhou@gmail.com>
2023-09-13 18:36:48 +0800

    format code

--- bin/zwm-on-xephyr.sh
+++ bin/zwm-on-xephyr.sh
@@ -2,31 +2,14 @@
 
 SCRIPT_ROOT=$(cd $(dirname "${BASH_SOURCE[0]}") && pwd)
 ZWM_ROOT=$(cd $SCRIPT_ROOT/.. && pwd)
-
-dep() {
-    sudo apt install xserver-xephyr
-}
-
-xephyr() {
-    local REZ=${1:-1280x720}
-    local DSP=${2:-:100}
-    
-    Xephyr $DSP -ac -br -screen $REZ -resizeable &
-    XEPHYR_PID=$!
-    sleep 2s
-
-    pushd $ZWM_ROOT
-    DISPLAY=$DSP ./bin/zwm-session
-    popd
-    
-    kill ${XEPHYR_PID}
-}
+REZ=${REZ:-1280x720}
+DSP=${DSP:-:100}
 
 usage() {
     prompt=$(basename $0)
     echo "
 Usage:
-$prompt {dep|help}
+$prompt {dep|prepare|run|clean|help}
 $prompt {Resulution} {DISPLAY}
 $prompt 1280x720
 $prompt 1280x720 :100
@@ -35,12 +18,23 @@ $prompt 1280x720 :100
 
 case "$1" in
     "dep" )
-        dep
+        sudo apt install xserver-xephyr
         ;;
-    "help" | "-h" )
-        usage
+    "prepare" )
+        shift
+        REZ=${1:-$REZ}
+        DSP=${2:-$DSP}
+        Xephyr $DSP -ac -br -screen $REZ -resizeable &
+        ;;
+    "run" )
+        pushd $ZWM_ROOT
+        DISPLAY=$DSP ./bin/zwm-session
+        popd
+        ;;        
+    "clean" )
+        killall Xephyr
         ;;
     * )
-        xephyr $@
+        usage
         ;;
 esac
