04d79eda00ab134ef804dd1f868ffc5699c430ae
Zachary Zhou <zchrzhou@gmail.com>
2023-09-13 18:58:19 +0800

    Format code

--- bin/zwm-on-xephyr.sh
+++ bin/zwm-on-xephyr.sh
@@ -5,11 +5,27 @@ ZWM_ROOT=$(cd $SCRIPT_ROOT/.. && pwd)
 REZ=${REZ:-1280x720}
 DSP=${DSP:-:100}
 
+prepare() {
+    REZ=${1:-$REZ}
+    DSP=${2:-$DSP}
+    Xephyr $DSP -ac -br -screen $REZ -resizeable &
+}
+
+run() {
+    pushd $ZWM_ROOT
+    DISPLAY=$DSP ./bin/zwm-session
+    popd
+}
+
+clean() {
+    killall Xephyr
+}
+
 usage() {
     prompt=$(basename $0)
     echo "
 Usage:
-$prompt {dep|prepare|run|clean|help}
+$prompt {dep|prepare|run|clean|help|-h}
 $prompt {Resulution} {DISPLAY}
 $prompt 1280x720
 $prompt 1280x720 :100
@@ -22,19 +38,21 @@ case "$1" in
         ;;
     "prepare" )
         shift
-        REZ=${1:-$REZ}
-        DSP=${2:-$DSP}
-        Xephyr $DSP -ac -br -screen $REZ -resizeable &
+        prepare $@
         ;;
     "run" )
-        pushd $ZWM_ROOT
-        DISPLAY=$DSP ./bin/zwm-session
-        popd
+        run
         ;;        
     "clean" )
-        killall Xephyr
+        clean
+        ;;
+    "-h" | "help" )
+        usage
         ;;
     * )
-        usage
+        prepare
+        sleep 2s
+        run
+        clean
         ;;
 esac
