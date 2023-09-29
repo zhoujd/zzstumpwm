80157da261df92ccecbf98ecf4bfe14da7a10ec3
Zachary Zhou <zchrzhou@gmail.com>
2023-09-29 11:36:55 +0800

    Update for Init

--- docker/run.sh
+++ docker/run.sh
@@ -44,13 +44,10 @@ prepare() {
 }
 
 init() {
-
-    docker run --name=${CTN_NAME} ${INIT_PARAM[@]} ${IMG}:${TAG} init
-}
-
-xephyr() {
-    XEPHYR_PARAM=(
-        -d
+    INIT_PARAM=(
+        --detach
+        --name=$CTN_NAME
+        --restart=always
         --privileged=true
         --cap-add=ALL
         --add-host=$HOST_NAME:$HOST_IP
@@ -77,13 +74,49 @@ xephyr() {
         -v $ZWM_TOP/.zwm/.vscode:$CTN_HOME/.vscode
     )
 
-    docker run --name=${CTN_NAME} ${XEPHYR_PARAM[@]} ${IMG}:${TAG} xephyr
+    docker run --name=${CTN_NAME} ${INIT_PARAM[@]} ${IMG}:${TAG} init
+}
+
+xephyr() {
+    XEPHYR_PARAM=(
+        --detach
+        --name=$CTN_NAME
+        --restart=always
+        --privileged=true
+        --cap-add=ALL
+        --add-host=$HOST_NAME:$HOST_IP
+        -e DISPLAY=$DISPLAY
+        -e GITHUB_TOKEN=$GITHUB_TOKEN
+        -e GITLAB_TOKEN=$GITLAB_TOKEN
+        -e ZWM_DSP=:110
+        -e ZWM_REZ=1920x1080
+        -h $CTN_HOST
+        -u $CTN_USER
+        -p $SSH_PORT:22
+        -v /dev:/dev
+        -v /tmp/.X11-unix:/tmp/.X11-unix
+        -v /var/run/docker.sock:/var/run/docker.sock
+        -v /etc/security/limits.conf:/etc/security/limits.conf
+        -v /etc/sysctl.conf:/etc/sysctl.conf
+        -v $ZWM_ROOT:$CTN_HOME/zzstumpwm
+        -v $ZWM_TOP/zzemacs:$CTN_HOME/zzemacs
+        -v $ZWM_TOP/lab:$CTN_HOME/lab
+        -v $ZWM_TOP/.zwm/.ssh:$CTN_HOME/.ssh
+        -v $ZWM_TOP/.zwm/.local:$CTN_HOME/.local
+        -v $ZWM_TOP/.zwm/.config:$CTN_HOME/.config
+        -v $ZWM_TOP/.zwm/.emacs.d:$CTN_HOME/.emacs.d
+        -v $ZWM_TOP/.zwm/.vscode:$CTN_HOME/.vscode
+    )
+
+    docker run ${XEPHYR_PARAM[@]} ${IMG}:${TAG} xephyr
 }
 
 zwm() {
     ZWM_DSP=${1:-$ZWM_DSP}
     RUN_PARAM=(
-        -d
+        --detach
+        --name=$CTN_NAME
+        --restart=always
         --privileged=true
         --cap-add=ALL
         --add-host=$HOST_NAME:$HOST_IP
@@ -107,7 +140,7 @@ zwm() {
         -v $ZWM_TOP/.zwm/.emacs.d:$CTN_HOME/.emacs.d
     )
 
-    docker run --name=${CTN_NAME} ${RUN_PARAM[@]} ${IMG}:${TAG} zwm
+    docker run ${RUN_PARAM[@]} ${IMG}:${TAG} zwm
 }
 
 stop() {
