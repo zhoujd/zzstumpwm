9516256339077244f840d360b78e8e601faa70f8
Zachary Zhou <zchrzhou@gmail.com>
2023-10-12 10:53:10 +0800

    Update for xephyr in docker

--- docker/xephyr/run.sh
+++ docker/xephyr/run.sh
@@ -9,16 +9,21 @@ CTN_NAME=${CTN_NAME:-$CTN_PREFIX-$TAG}
 USER_UID=$(id -u)
 USER_GID=$(id -g)
 
-XR_DSP=:110
+XR_DSP_NUM=120
+XR_DSP=:$XR_DSP_NUM
 XR_REZ=1280x720
 
 RUN_PARAM=(
     -d
     --privileged=true
     --cap-add=ALL
+    --ipc=host
+    --pid=host
     -e DISPLAY=$DISPLAY
     -u $USER_UID:$USER_GID
     -v /tmp/.X11-unix:/tmp/.X11-unix
+    -v /etc/passwd:/etc/passwd
+    -v /etc/group:/etc/group
 )
 
 XEPHYR_PARAM=(
@@ -39,6 +44,10 @@ stop() {
     docker rm ${CTN_NAME} >/dev/null 2>&1
 }
 
+shell() {
+    docker exec -it ${CTN_NAME} bash
+}
+
 logs() {
     docker logs ${CTN_NAME} 2>/dev/null
 }
@@ -48,6 +57,19 @@ status() {
     docker ps | grep ${CTN_NAME}
     echo "* Xephyr list"
     ps -ef | grep Xephyr | grep -v grep
+}
+
+clean() {
+    DSP_NUM=${1:-$XR_DSP_NUM}
+    XR_PID=$(ps -ef | grep Xephyr | grep :$DSP_NUM | awk '{ print $2 }')
+    if [ -n "$XR_PID" ]; then
+        kill -9 $XR_PID
+        echo "Kill $XR_PID for $DSP_NUM"
+    else
+        echo "No XR_PID for $DSP_NUM"
+    fi
+    sudo rm -rf /tmp/.X${DSP_NUM}-lock
+    sudo rm -rf /tmp/.X11-unix/X${DSP_NUM}
 }
 
 case $1 in
@@ -60,13 +82,20 @@ case $1 in
     stop )
         stop
         ;;
+    shell )
+        shell
+        ;;
     logs )
         logs
         ;;
     status )
         status
         ;;
+    clean )
+        shift
+        clean $@
+        ;;
     * )
-        echo "Usage: $(basename $0) {start|stop|logs|status}"
+        echo "Usage: $(basename $0) {start|stop|shell|logs|status|clean}"
         ;;
 esac
