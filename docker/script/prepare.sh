064041e8435afff075388907a24989bb57de34ea
Zachary Zhou <zchrzhou@gmail.com>
2023-10-13 18:19:48 +0800

    Update for zwm prepare

--- docker/script/prepare.sh
+++ docker/script/prepare.sh
@@ -4,10 +4,11 @@
 SCRIPT_ROOT=$(cd $(dirname "${BASH_SOURCE[0]}") && pwd)
 ZWM_ROOT=$(cd $SCRIPT_ROOT/../.. && pwd)
 ZWM_DATA=/opt/zwm
+ZWM_USER=$(id -u):$(id -g)
 
 zwm_create() {
     sudo mkdir -p $ZWM_DATA
-    sudo chown -R $(id -u):$(id -g) $ZWM_DATA
+    sudo chown -R $ZWM_USER $ZWM_DATA
     echo "Prepare: $ZWM_DATA Create Done"
 }
 
