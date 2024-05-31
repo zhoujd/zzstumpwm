2f0e602636b880eeb0a8d98ac85dceff0aa30a24
Zachary Zhou <zchrzhou@gmail.com>
2024-05-31 13:59:59 +0800

    Add font for default

--- stumpwmrc.d/font-setting.lisp
+++ stumpwmrc.d/font-setting.lisp
@@ -1,12 +1,14 @@
 ;;;; font setting -*-lisp-*-
 ;;tools: xlsfonts or xfontsel
-
-(in-package :zwm)
-
 ;;find a font of your choice in `xfontsel' and put it here.
 ;(set-font "*-unifont-medium-*-normal-*-16-*-*-*-*-*-*-*")
 ;(setf xft:*font-dirs* '("/run/current-system/profile/share/fonts/"))
 ;(setf clx-truetype:+font-cache-filename+ (concat (getenv "HOME") "/.fonts/font-cache.sexp"))
+
+(in-package :zwm)
+
+;;default font
+(set-font "12x24")
 
 #-clx-truetype
 (require 'clx-truetype)
@@ -14,11 +16,10 @@
 #-ttf-fonts
 (require 'ttf-fonts)
 
+;;ttf font
 (ignore-errors
- (or (and (xft:cache-fonts)
-          (set-font (make-instance 'xft:font
-                                   :family "WenQuanYi Micro Hei Mono"
-                                   :subfamily "Regular"
-                                   :size 12
-                                   :antialias t)))
-     (set-font "12x24")))
+ (set-font (make-instance 'xft:font
+                          :family "WenQuanYi Micro Hei Mono"
+                          :subfamily "Regular"
+                          :size 12
+                          :antialias t)))
