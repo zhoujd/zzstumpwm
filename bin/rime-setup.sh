a86a29539c2ad86918dfe007549da19c41b3c547
Zachary Zhou <zachary.zhou@yizhu-tech.com>
2024-06-24 17:30:17 +0800

    Update for rime

--- bin/rime-setup.sh
+++ bin/rime-setup.sh
@@ -4,7 +4,7 @@
 SCRIPT_ROOT=$(cd $(dirname "${BASH_SOURCE[0]}") && pwd)
 ZWM_ROOT=$(cd $SCRIPT_ROOT/.. && pwd)
 
-TARGET=~/.config/ibus/rime/ibus_rime.custom.yaml
-ln -sfvT $ZWM_ROOT/misc/.config/ibus/rime/default.custom.yaml $TARGET
+mkdir -p $TARGET
+ln -sfvT $ZWM_ROOT/misc/.config/ibus/rime/default.custom.yaml $TARGET/ibus_rime.custom.yaml
 
 echo "rime setup done"
