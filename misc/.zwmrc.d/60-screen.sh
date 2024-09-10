bffd52f7b98533f9e7076d527830e7c3560c04ea
Zachary Zhou <zchrzhou@gmail.com>
2024-09-10 23:07:47 +0800

    Update for screen

--- misc/.zwmrc.d/60-screen.sh
+++ misc/.zwmrc.d/60-screen.sh
@@ -16,20 +16,28 @@ screen() {
 
 vm() {
     local disp=Virtual1
-    local size=1920x1080
-    xrandr --output $disp  --mode $size
+    local size=1440x900
+    xrandr --output $disp --primary --mode $size
+}
+
+get_displays() {
+    xrandr -q | awk '($2 == "connected") {print $1;}'
 }
 
 main() {
-    local disp=Virtual1
-    local ext_flag=$(xrandr | grep "^$disp" | awk '{print $2}')
-    if [ "$ext_flag" = "connected" ]; then
-        echo "[screen] setup VM display"
-        vm
-    else
-        echo "[screen] setup laptop display"
-        screen
-    fi
+    case $(get_displays) in
+        Virtual1 )
+            echo "[screen] setup VM display"
+            vm
+            ;;
+        eDP-1 )
+            echo "[screen] setup laptop display"
+            screen
+            ;;
+        * )
+            echo "Unknow display"
+            ;;
+    esac
 }
 
 main
