d55dc8d0e93183d5807785d9573d5bffe7338203
Zachary Zhou <zchrzhou@gmail.com>
2024-11-03 19:33:39 +0800

    Update for zwm install script

--- bin/zwm-dm-setup.sh
+++ bin/zwm-dm-setup.sh
@@ -3,17 +3,23 @@
 ## run as root:
 #sudo ./zwm-dm-setup.sh
 
-SETUP_ROOT=`pwd`
+SCRIPT_ROOT=$(cd $(dirname "${BASH_SOURCE[0]}") && pwd)
 
 if [ $EUID -ne 0 ]; then
     echo "You must be a root user" 2>&1
     exit 1
 fi
 
-echo "[Desktop Entry]
+echo "Setup zwm desktop"
+tee /usr/share/xsessions/zwm.desktop <<EOF
+[Desktop Entry]
 Encoding=UTF-8
 Name=ZWM Session
 Comment=Runs the Zach Window Manager
 Exec=/etc/X11/Xsession
+Icon=
 Type=Application
-" > /usr/share/xsessions/zwm.desktop
+DesktopNames=ZWM
+EOF
+
+echo "Setup dwm dm done"
