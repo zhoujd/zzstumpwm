016ab26ae78bb32a6a23539b85cc6fea98135649
Zachary Zhou <zchrzhou@gmail.com>
2024-12-19 15:01:03 +0800

    Update me-setup.sh

--- bin/me-setup.sh
+++ bin/me-setup.sh
@@ -4,9 +4,10 @@ SCRIPT_ROOT=$(cd $(dirname "${BASH_SOURCE[0]}") && pwd)
 ZWM_ROOT=$(cd $SCRIPT_ROOT/.. && pwd)
 
 ## Install mec/mew
-TARGET=/usr/local/bin
-sudo cp -v $SCRIPT_ROOT/mec $TARGET
-sudo cp -v $SCRIPT_ROOT/mew $TARGET
+TARGET=~/.local/bin
+mkdir -p $TARGET
+cp -vf $SCRIPT_ROOT/mec $TARGET
+cp -vf $SCRIPT_ROOT/mew $TARGET
 
 ## Install mec/mew configure
 rm -rf ~/.config/jasspa
