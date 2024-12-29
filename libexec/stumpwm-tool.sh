fff62f860dd7b3a320b4e14a07c62fe25561b9eb
Zachary Zhou <zchrzhou@gmail.com>
2024-12-29 22:27:43 +0800

    Update for gtk tool lxappearance

--- libexec/stumpwm-tool.sh
+++ libexec/stumpwm-tool.sh
@@ -7,11 +7,13 @@ case $target in
         sudo apt install -y rofi keynav maim slop xclip xdotool
         sudo apt install -y sbcl
         sudo apt install -y acpi rlwrap wmctrl
+        sudo apt install -y lxappearance
         ;;
     arch|majaro )
         sudo pacman -S rofi keynav maim slop xclip xdotool
         sudo pacman -S sbcl
         sudo pacman -S acpi rlwrap wmctrl
+        sudo pacman -S lxappearance
         ;;
     * )
         echo "Use $0 [ubuntu|arch|manjaro]"
