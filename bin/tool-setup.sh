001e0591a7585d078cabcaa6c2aae456859b0a58
Zachary Zhou <zchrzhou@gmail.com>
2025-01-26 14:06:23 +0800

    Update for tools

--- bin/tool-setup.sh
+++ bin/tool-setup.sh
@@ -2,7 +2,7 @@
 
 SCRIPT_ROOT=$(cd $(dirname "${BASH_SOURCE[0]}") && pwd)
 ZWM_ROOT=$(cd $SCRIPT_ROOT/.. && pwd)
-TOOL_ROOT=$ZWM_ROOT/third-party/static-binaries
+TOOL_ROOT=$ZWM_ROOT/third-party/static-binaries/x86_64
 
 ## Install mec/mew
 TARGET=~/.local/bin
