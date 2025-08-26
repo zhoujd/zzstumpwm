fbda7c78bd12536709965ab533f5befc71d1983e
Zachary Zhou <zchrzhou@gmail.com>
2025-08-26 17:46:54 +0800

    Update font

--- stumpwmrc.d/font-setting.lisp
+++ stumpwmrc.d/font-setting.lisp
@@ -21,7 +21,7 @@
  (and
   (xft:cache-fonts)
   (set-font (make-instance 'xft:font
-                          :family "SF Mono SC"
+                          :family "WenQuanYi Micro Hei Mono"
                           :subfamily "Regular"
                           :size 11
                           :antialias t))))
