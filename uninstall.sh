3b768321434d9d58fba0f536acb6679fa441af9b
Zachary Zhou <zchrzhou@gmail.com>
2026-02-12 10:21:10 +0800

    Update uninstall.sh

--- uninstall.sh
+++ uninstall.sh
@@ -6,52 +6,40 @@ ZWM_TOP=$(cd $ZWM_ROOT/.. && pwd)
 
 echo "Uninstall zwm start ..."
 
-## Remove configure file and quicklisp
+## Remove lisp files
+rm -rf ~/common-lisp
 rm -f ~/.stumpwmrc
 rm -f ~/.clfswmrc
 rm -f ~/quicklisp
 rm -f ~/lisp
-
-## Remove common-lisp
-rm -rf ~/common-lisp
-
-## Remove SBCL configre
 rm -f ~/.sbclrc
-## Remove ECL configure
 rm -f ~/.eclrc
-## Remove CLISP configure
 rm -f ~/.clisprc
-## Remove Vim configure
+
+## Remove configure files
 rm -f ~/.vimrc
-## Remove Nano configure
 rm -f ~/.nanorc
-## Remove csh configure
 rm -f ~/.cshrc
-## Remove .xinitrc
 rm -f ~/.xinitrc
-## Remove .xsession
 rm -f ~/.xsession
-## Remove .xprofile
 rm -f ~/.xprofile
-## Remove .tmux.conf
 rm -f ~/.tmux.conf
-## Remove .screenrc
 rm -f ~/.screenrc
-## Remove .Xresources
 rm -f ~/.Xresources
-## Remove .Xdefaults
 rm -f ~/.Xdefaults
-## Remove .inputrc
 rm -f ~/.inputrc
-## Remove .urxvt
 rm -f ~/.urxvt
-## Remove .xrdb
 rm -f ~/.xrdb
-## Remove .keynavrc
 rm -f ~/.keynavrc
-## Remove .tigrc
 rm -f ~/.tigrc
-## Remove .tigrc.d
-rm -rf ~/.tigrc.d
+rm -f ~/.tigrc.d
+
+rm -f ~/.gtkrc-2.0
+rm -f ~/.tprofile
+rm -f ~/.vim
+rm -f ~/.Xmodmap.d
+rm -f ~/.zwmrc
+rm -f ~/.zwmrc.d
+
 
 echo "Uninstall zwm end ..."
